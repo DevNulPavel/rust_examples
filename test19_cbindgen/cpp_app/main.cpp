bf8378f16452c844848d749ba423dd65f17dd44f
DevNul <devnulpavel@gmail.com>
2019-12-30 14:17:35 +0300

    CBindGen example improvements

--- test19_cbindgen/cpp_app/main.cpp
+++ test19_cbindgen/cpp_app/main.cpp
@@ -1,8 +1,22 @@
 #include <library.h>
 #include <cstdio>
+#include <cstring>
 
-int main(int argc, char const *argv[]){    
-    int32_t result = function_1(10);
-    std::printf("Value from RUST: %d", result);
+int main(int argc, char const *argv[]){
+    {
+        int32_t result = rust_lib::function_1(10);
+        std::printf("Value from RUST: %d\n", result);
+    }
+
+    {
+        rust_lib::Buffer<int32_t> buffer;
+        std::memset(buffer.data, 0, sizeof(buffer.data));
+        buffer.data[0] = 10;
+        buffer.data[1] = 20;
+        buffer.data[2] = 20;
+        buffer.len = 3;
+        int32_t result = function_2(buffer);
+        std::printf("Value from RUST: %d\n", result);
+    }
     return 0;
 }
--- test19_cbindgen/src/lib.rs
+++ test19_cbindgen/src/lib.rs
@@ -1,8 +1,34 @@
+
+
+/// cbindgen:field-names=[data, len]
+/// cbindgen:derive-eq
+#[repr(C)]
+#[derive(Debug, Default)]
+pub struct Buffer<T> {
+    data_array: [T; 8],
+    len: usize,
+}
+
 #[no_mangle] 
 pub extern fn function_1(param: i32) -> i32 {
-    //println!("Value: {}", param);
+    println!("Value: {}", param);
     param
 }
+
+#[no_mangle] 
+pub extern fn function_2(buffer: Buffer<i32>) -> i32 {
+    println!("{:?}", buffer);
+    let mut result = 0 as i32;
+    for index in 0..buffer.len{
+        if let Some(val) = buffer.data_array.get(index) {
+            result += val;
+        }else{
+            break;
+        }
+    }
+    result
+}
+
 
 #[cfg(test)]
 mod tests {
