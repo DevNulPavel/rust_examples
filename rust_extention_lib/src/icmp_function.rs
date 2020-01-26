cba46838afaa23c89f2598b8607bbc54a2b7b56b
DevNul <devnulpavel@gmail.com>
2020-01-26 13:06:36 +0300

    Some improvement

--- rust_extention_lib/src/icmp_function.rs
+++ rust_extention_lib/src/icmp_function.rs
@@ -7,81 +7,96 @@ use std::cell::RefCell;
 
 #[no_mangle] 
 pub extern "C" fn icmp1_RUST_CODE(s2: *const c_char, s1: *const c_char) -> i32 {
-    if s2.is_null() {
-        return 0;
-    }
-
-    // Создаем Rust ссылочную строку из С-шной
-    // Если не смогли сконвертить в Rust строку - выходим
-    let s2_test_str = unsafe { CStr::from_ptr(s2) }.to_bytes();
-
-    // Защита от кривых данных
-    if s2_test_str.len() > 1024{
-        return 0;
-    }
-
-    struct Storrage{
-        buffer: Vec<u8>,
-        string_len: usize,
-    };
-    impl<'a> Storrage{
-        fn new() -> Storrage {
-            Storrage {
-                buffer: Vec::new(),
-                string_len: 0,
-            }
+    // Установка отлавливания паники слегка замедляет код, 
+    // если нужна максимальная скорость - можно убрать
+    // тогда производительность кода будет такая же
+    let result = std::panic::catch_unwind(move || -> i32 {
+        if s2.is_null() {
+            return 0;
         }
-    }
-
-    thread_local!{
-        static THREAD_STORRAGES: RefCell<Storrage> = RefCell::new(Storrage::new());
-    }
-
-    // Если есть исходная строка, тогда делаем ее в нижнем регистре
-    if s1.is_null() == false {
+    
         // Создаем Rust ссылочную строку из С-шной
         // Если не смогли сконвертить в Rust строку - выходим
-        let res = unsafe { CStr::from_ptr(s1) }.to_bytes();
-
+        let s2_test_str = unsafe { CStr::from_ptr(s2) }.to_bytes();
+    
         // Защита от кривых данных
-        if res.len() > 1024{
+        if s2_test_str.len() > 1024{
             return 0;
         }
-
-        THREAD_STORRAGES.with(|st| {
-            let storrage: &mut Storrage = &mut *st.borrow_mut();
-
-            // Увеличиваем размер буффера
-            if storrage.buffer.len() < res.len(){
-                storrage.buffer.resize(res.len(), 0);
+    
+        struct Storrage{
+            buffer: Vec<u8>,
+            string_len: usize,
+        };
+        impl<'a> Storrage{
+            fn new() -> Storrage {
+                Storrage {
+                    buffer: Vec::new(),
+                    string_len: 0,
+                }
             }
-
-            // Переводим в нижний регистр
-            let len = res.len();
-            for i in 0..len {
-                let byte = res[i];
-                let lowercase_byte = byte.to_ascii_lowercase();
-                storrage.buffer[i] = lowercase_byte;
+        }
+    
+        thread_local!{
+            static THREAD_STORRAGES: RefCell<Storrage> = RefCell::new(Storrage::new());
+        }
+    
+        // Если есть исходная строка, тогда делаем ее в нижнем регистре
+        if s1.is_null() == false {
+            // Создаем Rust ссылочную строку из С-шной
+            // Если не смогли сконвертить в Rust строку - выходим
+            let res = unsafe { CStr::from_ptr(s1) }.to_bytes();
+    
+            // Защита от кривых данных
+            if res.len() > 1024{
+                return 0;
             }
-            storrage.string_len = res.len();
-        });
-    }
-
-    return THREAD_STORRAGES.with(|st| {
-        let storrage: &Storrage = &(*st.borrow());
-        let length = storrage.string_len;
-
-        // Такой способ лучше, так как он не приводит к копированию
-        if let Some(bytes_slice) = storrage.buffer.get(0..length){
-            if s2_test_str.eq(bytes_slice) {
-                return 1;
+    
+            THREAD_STORRAGES.with(|st| {
+                let storrage: &mut Storrage = &mut *st.borrow_mut();
+    
+                // Увеличиваем размер буффера
+                if storrage.buffer.len() < res.len(){
+                    storrage.buffer.resize(res.len(), 0);
+                }
+    
+                // Переводим в нижний регистр
+                let len = res.len();
+                for i in 0..len {
+                    let byte = res[i];
+                    let lowercase_byte = byte.to_ascii_lowercase();
+                    storrage.buffer[i] = lowercase_byte;
+                }
+                storrage.string_len = res.len();
+            });
+        }
+    
+        return THREAD_STORRAGES.with(|st| {
+            let storrage: &Storrage = &(*st.borrow());
+            let length = storrage.string_len;
+    
+            // Такой способ лучше, так как он не приводит к копированию
+            if let Some(bytes_slice) = storrage.buffer.get(0..length){
+                if s2_test_str.eq(bytes_slice) {
+                    return 1;
+                }else{
+                    return 0;
+                }    
             }else{
                 return 0;
-            }    
-        }else{
-            return 0;
-        }
+            }
+        });
     });
+
+    // Обработка ошибки
+    match result {
+        Ok(res) =>{
+            res
+        }
+        Err(_) =>{
+            0
+        }
+    }
 }
 
 
