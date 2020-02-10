8412be4861eaf38d6456d9d04581b79d63faaf88
Pavel Ershov <pershov@game-insight.com>
2020-02-10 16:15:39 +0300

    Rayon example

--- test16_rayon/src/main.rs
+++ test16_rayon/src/main.rs
@@ -17,10 +17,45 @@ fn join_test(){
     });
 }
 
+fn partition(v: &mut [i32]) -> usize {
+    // Опорная точка - конец
+    let pivot = v.len() - 1;
+    let mut i = 0;
+    // Итерируемся от 0 до конца
+    for j in 0..pivot {
+        // Если текущее значение меньше конечного
+        if v[j] <= v[pivot] {
+            // Тогда меняем значение первого элемента и текущего
+            v.swap(i, j);
+            i += 1;
+        }
+    }
+    v.swap(i, pivot);
+    i
+}
+
+pub fn quick_sort(arr: &mut[i32]) {
+    if arr.len() <= 1 {
+        return;
+    }
+
+    let mid = partition(arr);
+    let (lo, hi) = arr.split_at_mut(mid);
+    rayon::join(|| quick_sort(lo), || quick_sort(hi));
+}
+
 fn main() {
-    let vec = vec![1, 2, 3, 4, 5];
-    let result = sum_of_squares(&vec);
-    println!("{}", result);
+    {
+        let vec = vec![1, 2, 3, 4, 5];
+        let result = sum_of_squares(&vec);
+        println!("{}", result);    
+    }
 
     join_test();
+
+    {
+        let mut vec = vec![3, 6, 1, 2, 8, 10, 23];
+        quick_sort(&mut vec);
+        assert_eq!(vec, [1, 2, 3, 6, 8, 10, 23]);
+    }
 }
