8047b2e27aa8f388516aa0a7a8a31d5526352cb7
Pavel Ershov <pershov@game-insight.com>
2020-02-19 13:21:10 +0300

    Small improvements in code

--- slack_direct_messenger/src/main.rs
+++ slack_direct_messenger/src/main.rs
@@ -1,3 +1,4 @@
+#![warn(clippy::all)]
 #![allow(dead_code)]
 
 #[macro_use(defer)] 
@@ -17,13 +18,42 @@ use std::io::Read;
 use std::io::Write;
 use std::path::PathBuf;
 use std::path::Path;
-use std::cmp::Ordering;
+//use std::cmp::Ordering;
 use std::collections::HashMap;
 use clap::{Arg, App};
 use serde::Deserialize;
 use serde::Serialize;
 use qrcode::QrCode;
 use image::Luma;
+
+// Тип используемых ошибок в коде
+#[derive(Debug)]
+enum MessageError{
+    EmptyEmail,
+    EmptyUser,
+    IsNotFound,
+    UsersListIsMissing,
+    ChannelDidNotOpen,
+    RequestError(reqwest::Error) // Обертка над ошибкой запроса
+}
+
+// Пустая автоматическая реализация
+impl std::error::Error for MessageError{
+}
+
+// Код для автоматической конвертации ошибки в коде
+impl std::fmt::Display for MessageError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+// Код для автоматической конвертации ошибки в коде
+impl From<reqwest::Error> for MessageError {
+    fn from(err: reqwest::Error) -> Self {
+        Self::RequestError(err)
+    }
+}
 
 
 // Создаем структурки, в которых будут нужные значения
@@ -34,10 +64,10 @@ struct UserInfo {
     real_name: Option<String>
 }
 
-async fn find_user_id_by_email(api_token: &str, email: &str) -> Result<String, Box<dyn std::error::Error>> {
+async fn find_user_id_by_email(client: &reqwest::Client, api_token: &str, email: &str) -> Result<String, MessageError> {
     // Проверяем наличие email
     if email.is_empty(){
-        return Err(Box::from("Is empty email"));
+        return Err(MessageError::EmptyEmail);
     }
 
     // Выполняем GET запрос
@@ -45,8 +75,8 @@ async fn find_user_id_by_email(api_token: &str, email: &str) -> Result<String, B
         ("token", api_token), 
         ("email", email)
     ];
-    let client = reqwest::Client::new();
-    let response = client.get("https://slack.com/api/users.lookupByEmail")
+    let response = client
+        .get("https://slack.com/api/users.lookupByEmail")
         .query(&get_parameters)
         .send()
         .await?;
@@ -74,11 +104,11 @@ async fn find_user_id_by_email(api_token: &str, email: &str) -> Result<String, B
         return Ok(response_json.user.id);
     }
 
-    return Err(Box::from(format!("User is not found for this email: {}", email)));
+    Err(MessageError::IsNotFound)
 }
 
 // Result<Vec<UserInfo>, Box<dyn std::error::Error>>
-async fn request_all_slack_users(api_token: &str) -> Vec<UserInfo> {   
+async fn request_all_slack_users(client: &reqwest::Client, api_token: &str) -> Vec<UserInfo> {   
     // Создаем структурки, в которых будут нужные значения
     #[derive(Deserialize, Debug)]
     struct Metadata {
@@ -106,15 +136,14 @@ async fn request_all_slack_users(api_token: &str) -> Vec<UserInfo> {
 
         //println!("{:?}", get_parameters);
 
-        // Выполняем GET запрос
-        let client = reqwest::Client::new();
+        // Выполняем GET запрос 
         let request_res = client.get("https://slack.com/api/users.list")
             .query(&get_parameters)
             .send()
             .await;
 
         //println!("{:?}", request_res);
-        
+       
         // Если сервер ответил
         if let Ok(response) = request_res {
             // Пробуем распарсить результат
@@ -126,7 +155,7 @@ async fn request_all_slack_users(api_token: &str) -> Vec<UserInfo> {
 
             if let Ok(json) = parse_res.as_mut() {
                 //println!("{:?}", json);
-                if json.ok == true {
+                if json.ok {
                     //println!("{:?}", json);
 
                     // Обрабатываем пользователей
@@ -140,7 +169,7 @@ async fn request_all_slack_users(api_token: &str) -> Vec<UserInfo> {
                     // Обновляем курсор, если он есть
                     last_cursor = if let Some(meta) = json.response_metadata.take().as_mut() {
                         if let Some(cursor) = meta.next_cursor.take() {
-                            if cursor.is_empty() == false {
+                            if !cursor.is_empty() {
                                 Some(cursor)
                             }else{
                                 break;
@@ -171,53 +200,60 @@ async fn request_all_slack_users(api_token: &str) -> Vec<UserInfo> {
 }
 
 // TODO: Поправить возвращаемое значение
-async fn find_user_id_by_name(api_token: &str, src_user_name: &str) -> Result<String, Box<dyn std::error::Error>> {
+async fn find_user_id_by_name(client: &reqwest::Client, api_token: &str, src_user_name: &str) -> Result<String, MessageError> {
     // Проверяем наличие user
     if src_user_name.is_empty(){
-        return Err(Box::from("Is empty user"));
+        return Err(MessageError::EmptyUser);
     }
 
+    // Переводим имя в нижний регистр
     let user = src_user_name.to_lowercase();
         
-    let cache_file_folder = PathBuf::new().join(dirs::home_dir().unwrap()).join(".cache/slack_direct_messenger/");
+    // Пути к папке с кешем пользователей
+    let cache_file_folder = PathBuf::new()
+        .join(dirs::home_dir().unwrap())
+        .join(".cache/slack_direct_messenger/");
     let cache_file_name = Path::new("users_cache.json");    
-    let cache_file_full_path = PathBuf::new().join(&cache_file_folder).join(&cache_file_name);
+    let cache_file_full_path = PathBuf::new()
+        .join(&cache_file_folder)
+        .join(&cache_file_name);
 
-    let mut users_cache: HashMap<String, UserInfo> = match File::open(&cache_file_full_path){
-        Ok(mut file) => {
+    // Читаем данные из файлика, если не удалось - просто создаем новый
+    let mut users_cache: HashMap<String, UserInfo> = File::open(&cache_file_full_path)
+        .and_then(|mut file: std::fs::File|{
             let mut data = String::new();
-            match file.read_to_string(&mut data) {
-                Ok(_)=> {
-                    match serde_json::from_str::<HashMap<String, UserInfo>>(data.as_str()){
-                        Ok(result) => result,
-                        Err(_) => HashMap::new(),
-                    }
-                },
-                Err(_) => HashMap::new()
-            }        
-        },
-        Err(_)=> HashMap::new()
-    };
+            file.read_to_string(&mut data)?;
+            Ok(data)
+        })
+        .and_then(|data: String|{
+            let result = serde_json::from_str::<HashMap<String, UserInfo>>(&data)?;
+            Ok(result)
+        })
+        .unwrap_or_default();
 
     // Ищем в кеше
     if let Some(found) = users_cache.get(&user){
         return Ok(found.id.clone()); // TODO: Избавиться от клонирования, можно мувить результат
     }
 
+    // Создаем новый объект результата
     let mut found_info: Option<UserInfo> = None;
 
-    let users_list = request_all_slack_users(api_token).await;
-    if users_list.len() == 0 {
-        return Err(Box::from("Empty users list"));
+    // Получаем список юзеров
+    let users_list = request_all_slack_users(client, api_token).await;
+    if users_list.is_empty() {
+        return Err(MessageError::UsersListIsMissing);
     }
 
+    // Структура юзера с приоритетом
     struct UserInfoWithPriority{
         priority: i32,
         info: UserInfo,
     }
+
     let mut found_users: Vec<UserInfoWithPriority> = Vec::new(); 
 
-    let search_parts: Vec<&str> = user.split(" ").collect();
+    let search_parts: Vec<&str> = user.split(' ').collect();
     for user_info in users_list { // Объекты будут перемещать владение в user_info
         // Проверяем короткое имя
         if user_info.name == user {
@@ -237,7 +273,7 @@ async fn find_user_id_by_name(api_token: &str, src_user_name: &str) -> Result<St
                         priority: 0,
                         info: user_info,
                     };
-                    let test_name_components = real_name.split(" "); // split создает итератор с &str
+                    let test_name_components = real_name.split(' '); // split создает итератор с &str
                     for test_part in test_name_components { // Здесь у нас владение перемещается полностью в итерируемые элементы, test_name_components пустой после
                         for search_part in search_parts.iter() { // Тут мы итерируемся по элементам
                             if test_part == *search_part {
@@ -259,25 +295,28 @@ async fn find_user_id_by_name(api_token: &str, src_user_name: &str) -> Result<St
         users_cache.insert(user.to_owned(), info.clone());
         result_found_user = Some(info);
     }else{
-        found_users.sort_by(|val1, val2|-> Ordering {
+        found_users.sort_by_key(|user: &UserInfoWithPriority|{
+            user.priority
+        });
+        /*found_users.sort_by(|val1, val2|-> Ordering {
             if val1.priority > val2.priority{
                 return Ordering::Greater;
             } else if val1.priority < val2.priority{
                 return Ordering::Less;
             }
             return Ordering::Equal;
-        });
+        });*/
 
         for user_info in found_users {
-            users_cache.insert(user.to_owned(), user_info.info.clone()); // TODO: !!!
+            users_cache.insert(user.clone(), user_info.info.clone()); // TODO: !!!
             result_found_user = Some(user_info.info);
         }
     }
 
+    // Сохраняем наш кэш
     if let Some(user_info) = result_found_user{
-        if cache_file_folder.exists() == false{
-            if std::fs::create_dir_all(cache_file_folder).is_err() {
-            }
+        if !cache_file_folder.exists() {
+            std::fs::create_dir_all(cache_file_folder).unwrap();
         }
         if let Ok(mut file) = File::open(&cache_file_full_path) {
             if let Ok(json_text) = serde_json::to_string(&users_cache){
@@ -286,18 +325,17 @@ async fn find_user_id_by_name(api_token: &str, src_user_name: &str) -> Result<St
             }
         }
 
-        return Ok(user_info.id.clone());
+        return Ok(user_info.id);
     }
 
-    return Err(Box::from("User id is not found"));
+    Err(MessageError::IsNotFound)
 }
 
-async fn open_direct_message_channel(api_token: &str, user_id: &str) -> Result<String, Box<dyn std::error::Error>>{
+async fn open_direct_message_channel(client: &reqwest::Client, api_token: &str, user_id: &str) -> Result<String, MessageError>{
     // Выполняем POST запрос
     let post_params = vec![
         ("user", user_id),
     ];
-    let client = reqwest::Client::new();
     let response = client.post("https://slack.com/api/im.open")
         .bearer_auth(api_token)
         .form(&post_params)
@@ -327,16 +365,15 @@ async fn open_direct_message_channel(api_token: &str, user_id: &str) -> Result<S
         return Ok(response_json.channel.id);
     }
 
-    Err(Box::from("Direct message channel opening failed"))
+    Err(MessageError::ChannelDidNotOpen)
 }
 
-async fn send_direct_message_to_channel(api_token: &str, channel: &str, text: &str) -> Result<(), Box<dyn std::error::Error>>{
+async fn send_direct_message_to_channel(client: &reqwest::Client, api_token: &str, channel: &str, text: &str) -> Result<(), MessageError>{
     // Выполняем POST запрос
     let post_params = vec![
         ("channel", channel),
         ("text", text)
     ];
-    let client = reqwest::Client::new();
     client.post("https://slack.com/api/chat.postMessage")
         .bearer_auth(api_token)
         .form(&post_params)
@@ -347,7 +384,7 @@ async fn send_direct_message_to_channel(api_token: &str, channel: &str, text: &s
     Ok(())
 }
 
-async fn send_qr_to_channel(api_token: &str, channel: &str, qr_text: &str, qr_commentary: &str) -> Result<(), Box<dyn std::error::Error>>{
+async fn send_qr_to_channel(client: &reqwest::Client, api_token: &str, channel: &str, qr_text: &str, qr_commentary: &str) -> Result<(), MessageError>{
     // Encode some data into bits.
     let code = QrCode::new(qr_text.as_bytes()).unwrap();
 
@@ -358,8 +395,10 @@ async fn send_qr_to_channel(api_token: &str, channel: &str, qr_text: &str, qr_co
     let new_uuid = uuid::Uuid::new_v4();
     let filename = format!("{}.png", new_uuid);
     let temp_dir = std::env::temp_dir();
-    let result_file_path = PathBuf::new().join(temp_dir).join(filename.as_str());
-    println!("{:?}", result_file_path);
+    let result_file_path = PathBuf::new()
+        .join(temp_dir)
+        .join(filename.as_str());
+    //println!("{:?}", result_file_path);
 
     // Save the image.
     image.save(&result_file_path).unwrap();
@@ -368,24 +407,18 @@ async fn send_qr_to_channel(api_token: &str, channel: &str, qr_text: &str, qr_co
     defer!(std::fs::remove_file(&result_file_path).unwrap());
 
     // TODO: Читаем файлик
-    let file_data: Vec<u8> = match File::open(&result_file_path){
-        Ok(mut file) => {
+    let file_data: Vec<u8> = File::open(&result_file_path)
+        .and_then(|mut file|{
             let mut data: Vec<u8> = Vec::new();
-            match file.read_to_end(&mut data) {
-                Ok(_)=> {
-                    data
-                },
-                Err(_) => {
-                    data
-                }
-            }        
-        },
-        Err(_)=> Vec::new()
-    };
+            file.read_to_end(&mut data)?;
+            Ok(data)
+        })
+        .unwrap(); // Если не прочитали файлик - падаем
+
     //println!("{:?}", file_data);
 
-    // Выполняем POST запрос
-    let commentary = match qr_commentary.len(){
+    // Есть или нет комментарий?
+    let commentary = match qr_commentary.len() {
         0 => qr_commentary,
         _ => qr_text
     };
@@ -399,30 +432,27 @@ async fn send_qr_to_channel(api_token: &str, channel: &str, qr_text: &str, qr_co
         ("filename", filename.to_owned()),
     ];
     let form = Form::new()
-        //.part("channel", Part::text(channel.to_owned()))
-        //.part("initial_comment", Part::text(commentary.to_owned()))
-        //.part("filename", Part::text(filename.to_owned()))
+        .part("channel", Part::text(channel.to_owned()))
+        .part("initial_comment", Part::text(commentary.to_owned()))
+        .part("filename", Part::text(filename.to_owned()))
         .part("file", Part::bytes(file_data))
-        //.part("content", Part::stream(file_data))
+        // .part("content", Part::stream(file_data))
         .percent_encode_attr_chars();
-    println!("{}", form.boundary());
+    //println!("{}", form.boundary());
 
-    let client = reqwest::Client::new();
-    let response = client.post("https://slack.com/api/files.upload")
+    let _response = client.post("https://slack.com/api/files.upload")
         .bearer_auth(api_token)
         .form(&post_params)
         .multipart(form)
         .send()
         .await?;
-    println!("{:?}", response);
+    //println!("{:?}", _response);
 
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO: Использовать клиент для переиспользования соединений
-
     // Parse parameters
     let matches = App::new("slack_direct_messenger")
                             .version("1.0")
@@ -460,10 +490,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Api token
     let api_token = std::env::var("SLACK_API_TOKEN").expect("SLACK_API_TOKEN environment variable is missing");
 
-    let id = match find_user_id_by_email(api_token.as_str(), email).await {
+    // Создаем клиента для переиспользования
+    let client = reqwest::Client::new();
+
+    // Ищем id
+    let id = match find_user_id_by_email(&client, &api_token, email).await {
         Ok(id) => id,
         Err(_)=>{
-            match find_user_id_by_name(api_token.as_str(), user).await {
+            match find_user_id_by_name(&client, &api_token, user).await {
                 Ok(id) => id,
                 Err(err) => {
                     println!("{}", err);
@@ -472,18 +506,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
     };
-    println!("{}", id);
+    //println!("{}", id);
 
-    let channel_id = open_direct_message_channel(api_token.as_str(), id.as_str()).await?;
-    println!("{}", channel_id);
+    // Открываем канал для сообщений
+    let channel_id = open_direct_message_channel(&client, &api_token, &id).await?;
+    //println!("{}", channel_id);
     
-    if text.len() > 0 {
-        // String можно преобразовать в &String, затем вызовется deref() -> &str
-        send_direct_message_to_channel(&api_token, &channel_id, text).await?;
+    if !text.is_empty() {
+        // String можно преобразовать в &String, затем вызовется as_ref() -> &str
+        send_direct_message_to_channel(&client, &api_token, &channel_id, text).await?;
     }
 
-    if qr_text.len() > 0{
-        send_qr_to_channel(&api_token, &channel_id, qr_text, qr_commentary).await?;
+    if !qr_text.is_empty() {
+        send_qr_to_channel(&client, &api_token, &channel_id, qr_text, qr_commentary).await?;
     }
 
     Ok(())
