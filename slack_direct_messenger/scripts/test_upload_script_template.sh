3713fc72273d2e0a44c158d6b4a1d72fa6a1b25c
Pavel Ershov <pershov@game-insight.com>
2020-02-19 13:21:34 +0300

    Small improvements in code (2)

--- slack_direct_messenger/scripts/test_upload_script_template.sh
+++ slack_direct_messenger/scripts/test_upload_script_template.sh
@@ -3,7 +3,8 @@
 export SLACK_API_TOKEN=""
 
 # Slack message
-./target/debug/slack_direct_messenger \
+#./target/debug/slack_direct_messenger \
+cargo run -- \
     --slack_user_email "" \
     --slack_user "" \
     --slack_user_text "" \
