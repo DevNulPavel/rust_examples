f6289a02a4b54489440d6f8d026239c8cc278555
Pavel Ershov <pershov@game-insight.com>
2020-02-25 19:01:15 +0300

    Habr parser improvement

--- habr_headers/src/main.rs
+++ habr_headers/src/main.rs
@@ -25,13 +25,24 @@ use prettytable::{Table, Row, Cell, Attr};
 
 const CACHE_FILE_NAME: &str = "habrahabr_headers.json";
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//type CSSErr = scraper::cssparser::ParseError<'i, SelectorParseErrorKind<'i>>;
 macro_rules! ToCSSError {
     ($expression:expr) => {
         $expression.map_err(|err|{
             HabrErrors::CSSParseError(format!("{:?}", err))
         })
+    };
+}
+
+macro_rules! ToTextOption {
+    ($expression:expr) => {
+        // Так как текст - это итератор, то нужно сначала создавать итератор, который может проверять следующий элемент
+        // Затем пробовать этот элемент
+        match $expression.text().peekable().peek() {
+            Some(_) => Some($expression.text().collect()),
+            None => None
+        }
     };
 }
 
@@ -61,6 +72,7 @@ impl Debug for HabrErrors{
 #[derive(Debug)] // Автоматический debug вывод
 struct HabrTitle{
     time: String,
+    tags: Vec<String>,
     title: String,
     link: String
 }
@@ -68,6 +80,53 @@ impl Display for HabrTitle{
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}, {} ({})", self.title, self.time, self.link)
     }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct CssSelectors{
+    preview_selector: scraper::Selector,
+    time_selector: scraper::Selector,
+    tags_selector: scraper::Selector,
+    link_selector: scraper::Selector,
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+fn text_to_multiline(text: &str, words_count: usize, intent: Option<&str>) -> String{
+    let mut line_words_count = 0;
+    let mut multiline_text: String = text
+        .split(' ')
+        .map(|word|{
+            if let Some(last) = word.chars().last(){
+                if last == '\n'{
+                    line_words_count = 0;
+                }
+            }
+
+            line_words_count += 1;
+
+            if line_words_count % words_count != 0 {
+                vec![word, " "]
+            }else if let Some(intent) = intent{
+                vec![word, "\n", intent]
+            }else{
+                vec![word, "\n"]
+            }
+        })
+        .flatten()
+        .collect();
+
+    // Убираем пробел или \n в конце
+    while let Some(last) = multiline_text.chars().last() {
+        if last.is_whitespace(){
+            multiline_text.pop();
+        }else{
+            break;
+        }
+    }
+    
+    multiline_text
 }
 
 fn print_results(selected: &[HabrTitle], previous_results: Option<HashSet<String>>){
@@ -90,34 +149,29 @@ fn print_results(selected: &[HabrTitle], previous_results: Option<HashSet<String
     // Выводим текст, используем into_iter для потребляющего итератора
     for info in selected.iter().rev() {
         // TODO: Может можно оптимальнее??
-        let mut multiline_text: String = info.title
-            .split(' ')
-            .enumerate()
-            .map(|(i, word)|{
-                if (i+1) % 5 != 0 {
-                    vec![word, " "]
-                }else{
-                    vec![word, "\n"]
-                }
-            })
-            .flatten()
-            .collect();
-        multiline_text.pop();
+
+        let multiline_text = text_to_multiline(&info.title, 3, None);
+        // multiline_text.push_str("\n\n");
+        // multiline_text.push_str(&info.tags);
+
+        let tags_text = text_to_multiline(&info.tags.join("\n"), 3, Some(" "));
 
         let text_color = previous_results
             .as_ref()
             .map(|set|{
                 if set.contains(info.link.as_str()) {
-                    color::YELLOW
-                }else{
                     color::GREEN
+                }else{
+                    color::YELLOW
                 }
             })
-            .unwrap_or(color::GREEN);
+            .unwrap_or(color::YELLOW);
 
         let row = Row::new(vec![
                 Cell::new(&multiline_text)
                     .with_style(Attr::ForegroundColor(text_color)),
+                Cell::new(&tags_text)
+                    .with_style(Attr::ForegroundColor(color::WHITE)),
                 Cell::new(&info.time)
                     .with_style(Attr::ForegroundColor(color::WHITE)),
                 Cell::new(&info.link)
@@ -129,7 +183,7 @@ fn print_results(selected: &[HabrTitle], previous_results: Option<HashSet<String
     table.printstd();
 }
 
-fn request_links(link: &str) -> Result<Vec<HabrTitle>, HabrErrors>{
+fn request_links_from_page(link: &str, shared_selectors: &CssSelectors) -> Result<Vec<HabrTitle>, HabrErrors>{
     let page_text = get(link)?
         .text()?;
 
@@ -137,58 +191,55 @@ fn request_links(link: &str) -> Result<Vec<HabrTitle>, HabrErrors>{
     let parsed = Html::parse_document(&page_text);
     drop(page_text);
 
-    // Создаем селектор по классу
-    let preview_selector = ToCSSError!(Selector::parse(".post.post_preview"))?;
-    let time_selector = ToCSSError!(Selector::parse(".post__time"))?;
-    let link_selector = ToCSSError!(Selector::parse(".post__title_link"))?;
-
     // https://docs.rs/scraper/0.11.0/scraper/element_ref/struct.ElementRef.html
     let selected: Vec<HabrTitle> = parsed
-        .select(&preview_selector)
+        .select(&shared_selectors.preview_selector)
         .map(|preview_element|{
-            let time = preview_element.select(&time_selector).take(1).next();
-            let link = preview_element.select(&link_selector).take(1).next();
-            (time, link)
+            let time = preview_element.select(&shared_selectors.time_selector).take(1).next();
+            let link = preview_element.select(&shared_selectors.link_selector).take(1).next();
+            (time, link, preview_element)
         })
-        .filter(|(time, link)|{
+        .filter(|(time, link, _)|{
             time.is_some() && link.is_some()
         })
-        .map(|(time, link)|{
-            (time.unwrap(), link.unwrap())
+        .map(|(time, link, preview_element)|{
+            (time.unwrap(), link.unwrap(), preview_element)
         })
-        .map(|(time, link)|{
-            // TODO: В макрос
-            // Так как текст - это итератор, то нужно сначала создавать итератор, который может проверять следующий элемент
-            // Затем пробовать этот элемент
-            let time: Option<String> = match time.text().peekable().peek() {
-                Some(_) => Some(time.text().collect()),
-                None => None
-            };
-
-            // TODO: В макрос
-            // Так как текст - это итератор, то нужно сначала создавать итератор, который может проверять следующий элемент
-            // Затем пробовать этот элемент
-            let text: Option<String> = match link.text().peekable().peek() {
-                Some(_) => Some(link.text().collect()),
-                None => None
-            };
+        .map(|(time, link, preview_element)|{
+            let time: Option<String> = ToTextOption!(time);
+            let text: Option<String> = ToTextOption!(link);
 
             let href = link.value().attr("href");
 
-            (time, href, text)
+            (time, href, text, preview_element)
         })
-        .filter(|(time, href, text)|{
-            let valid_time = time.is_some();
-            let valid_text = text.is_some();
-            let valid_href = href.is_some();
-            valid_time && valid_href && valid_text
+        .filter(|(time, href, text, _)|{
+            time.is_some() && text.is_some() && href.is_some()
         })
-        .map(|(time, href, text)|{
+        .map(|(time, href, text, preview_element)|{
             let time = time.unwrap();
             let text = text.unwrap();
             let href = href.unwrap().to_owned();
+
+            // TODO: Может быть можно улучшить???
+            let tags: Vec<String> = preview_element.select(&shared_selectors.tags_selector)
+                .map(|element|{
+                    let text: Option<String> = ToTextOption!(element);
+                    text
+                })
+                .filter(|val|{
+                    val.is_some()
+                })
+                .map(|val|{
+                    format!("#{}", val.unwrap())
+                })
+                .collect();
+            
+            //let tags_str = tags.join("\n");
+
             HabrTitle{
                 time,
+                tags,
                 title: text,
                 link: href
             }
@@ -199,17 +250,27 @@ fn request_links(link: &str) -> Result<Vec<HabrTitle>, HabrErrors>{
 }
 
 fn receive_habr_info() -> Vec<HabrTitle>{
-    const LINKS: [&str; 3] = [
+    const LINKS: [&str; 4] = [
         "https://habr.com/ru/all/",
         "https://habr.com/ru/all/page2/",
-        "https://habr.com/ru/all/page3/"
+        "https://habr.com/ru/all/page3/",
+        "https://habr.com/ru/all/page4/",
     ];
+
+    // Создаем селекторы по классу заранее
+    let selectors = CssSelectors{
+        preview_selector: ToCSSError!(Selector::parse(".post.post_preview")).unwrap(),
+        time_selector: ToCSSError!(Selector::parse(".post__time")).unwrap(),
+        tags_selector: ToCSSError!(Selector::parse(".inline-list__item-link.hub-link")).unwrap(),
+        link_selector: ToCSSError!(Selector::parse(".post__title_link")).unwrap(),
+    };
 
     // Обходим параллельно все ссылки
     let selected: Vec<HabrTitle> = LINKS
         .par_iter()
+        // Код ниже исполняется параллельно
         .map(|link: &&str| -> Result<Vec<HabrTitle>, HabrErrors> {
-            request_links(*link)
+            request_links_from_page(*link, &selectors)
         })
         // Фильтруем проблемные
         .filter(|value|{
