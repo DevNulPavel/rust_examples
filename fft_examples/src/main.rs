3995794294d5f90ca637a31c88244f93e3ecaead
Pavel Ershov <pershov@game-insight.com>
2020-02-27 15:54:50 +0300

    Plot support

--- fft_examples/src/main.rs
+++ fft_examples/src/main.rs
@@ -3,12 +3,14 @@
 
 // Perform a forward FFT of size 1234
 extern crate rustfft;
+extern crate plotters;
 
-use std::sync::Arc;
+//use std::sync::Arc;
 use rustfft::FFTplanner;
 use rustfft::num_complex::Complex;
 use rustfft::num_complex::Complex32;
 use rustfft::num_traits::Zero;
+use plotters::prelude::*;
 
 fn print_data(prefix: &str, data: &[Complex32]){
     data
@@ -33,6 +35,64 @@ fn print_result(prefix: &str, data: &[Complex32]){
         });
 }
 
+fn plot_results(input: &[Complex32], out: &[Complex32]) -> Result<(), Box<dyn std::error::Error>> {
+    let data_size = out.len() / 2;
+
+    let in_iter = input
+        .iter()
+        .enumerate()
+        .map(|(i, val)|{
+            (i as f32, val.re)
+        });
+    
+    let out_iter = out
+        .iter()
+        .take(data_size)
+        .enumerate()
+        .map(|(i, val)|{
+            let res = (1.0 / out.len() as f32) * (val.re * val.re + val.im * val.im).sqrt();
+            (i as f32, res)
+        });
+
+
+    let root = BitMapBackend::new("0.png", (640, 480)).into_drawing_area();
+    root.fill(&WHITE)?;
+    
+    let mut chart = ChartBuilder::on(&root)
+        .margin(5)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_ranged(0.0f32..data_size as f32, -1.0f32..1.0f32)?;
+
+    chart.configure_mesh().draw()?;
+
+    //let test_iter = (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x));
+
+    chart
+        .draw_series(LineSeries::new(
+            in_iter,
+            &BLUE,
+        ))?
+        .label("Input signal")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
+
+    chart
+        .draw_series(LineSeries::new(
+            out_iter,
+            &RED,
+        ))?
+        .label("Freq amplitudes")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+
+    Ok(())
+}
+
 fn make_test_input()-> Vec<Complex32> {
     let input: Vec<Complex32> = vec![
         Complex32::new(1.0, 0.0),
@@ -47,35 +107,46 @@ fn make_test_input()-> Vec<Complex32> {
     input
 }
 
+fn make_harm(number: u32, power: f32, i: usize, data_size: usize) -> Complex32 {
+    let step: f32 = std::f32::consts::PI * 2.0 * number as f32 / (data_size as f32);
+    let result: Complex32 = Complex32::new((step * i as f32).sin() * power, 0.0);
+    result
+}
+
 fn test_forward_transform_with_planner(){
-    const DATA_SIZE: usize = 128;
+    const DATA_SIZE: usize = 256;
 
     // Предаллоцируем вектор нужного размера с нулевыми значениями
-    let mut i = 0 as f32;
-    let mut input:  Vec<Complex32> = std::iter::repeat_with(||{ 
-            // Синус обновляется целиком 2 раза, это значит, что в спектре будет 2я гармоника
-            const HARMONIC_NUMBER1: f32 = 3.0;
-            const HARMONIC_NUMBER2: f32 = 5.0;
-
-            const HARMONIC_POWER_1: f32 = 0.7;
-            const HARMONIC_POWER_2: f32 = 0.3;
-
-            const STEP1: f32 = std::f32::consts::PI * 2.0 * HARMONIC_NUMBER1 / (DATA_SIZE as f32);
-            const STEP2: f32 = std::f32::consts::PI * 2.0 * HARMONIC_NUMBER2 / (DATA_SIZE as f32);
-
+    let mut input:  Vec<Complex32> = std::iter::repeat(0.0f32)
+        .enumerate()
+        .map(|(i, val)|{
+            (i, val, 0.0f32)
+        })
+        .map(|(i, val, step)|{
+            let result: Complex32 = make_harm(4, 0.4, i, DATA_SIZE);
+            (i, val + result, step + 1.0f32)
+        })
+        .map(|(i, val, step)|{
+            let result: Complex32 = make_harm(13, 0.8, i, DATA_SIZE);
+            (i, val + result, step + 1.0f32)
+        })
+        .map(|(i, val, step)|{
+            let result: Complex32 = make_harm(27, 0.4, i, DATA_SIZE);
+            (i, val + result, step + 1.0f32)
+        })
+        .map(|(i, val, step)|{
+            let result: Complex32 = make_harm(40, 0.8, i, DATA_SIZE);
+            (i, val + result, step + 1.0f32)
+        })
+        .map(|(_, val, step)|{
             // Не забываем сделать нормализацию
-            let result: Complex32 = Complex32::new(((STEP1 * i).sin() * HARMONIC_POWER_1 + 
-                                                    (STEP2 * i).sin() * HARMONIC_POWER_2) / 2.0, 0.0);
-
-            i += 1.0;
-
-            result
+            Complex32::new(val.re / step, 0.0)
         })
         .take(DATA_SIZE)
         .collect();
     let mut output: Vec<Complex32> = vec![Complex::zero(); DATA_SIZE];
     
-    print_data("Input", &input);
+    //print_data("Input", &input);
 
     // FFTplanner позволяет выбирать оптимальный алгоритм работы для входного размера данных
     let mut planner = FFTplanner::new(false);
@@ -93,7 +164,9 @@ fn test_forward_transform_with_planner(){
     //let fft_clone = Arc::clone(&fft);
 
     println!("\n\n");
-    print_result("Output", &output);
+    //print_result("Output", &output);
+
+    plot_results(&input, &output).ok();
 }
 
 fn main() {
