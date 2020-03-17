3271a5f40f803058a0481d7a3f8eb8cf7593d1ac
DevNul <devnulpavel@gmail.com>
2020-03-17 21:42:00 +0300

    Some improvements in code

--- test32_vst_plugin/src/lib.rs
+++ test32_vst_plugin/src/lib.rs
@@ -10,20 +10,26 @@ use vst::plugin::{
     PluginParameters,
     Category
 };
+use rustfft::FFTplanner;
+use rustfft::num_traits::Zero;
+use rustfft::num_complex::{
+    Complex,
+    Complex32
+};
 
 
 ////////////////////////////////////////////////////////////
 
 struct SimplePluginParameters{
     volume: std::sync::Mutex<f32>,
-    threshold: std::sync::Mutex<f32>
+    threshold: std::sync::atomic::AtomicU32
 }
 
 impl Default for SimplePluginParameters {
     fn default() -> SimplePluginParameters {
         SimplePluginParameters {
             volume: std::sync::Mutex::from(1.0_f32),
-            threshold: std::sync::Mutex::from(1.0_f32)
+            threshold: std::sync::atomic::AtomicU32::from(1_000_000)
         }
     }
 }
@@ -59,11 +65,10 @@ impl PluginParameters for SimplePluginParameters {
         match index {
             // Convert to a percentage
             0 => {
-                if let Ok(val) = self.threshold.lock() {
-                    format!("{}", *val * 100.0)
-                }else{
-                    "".to_string()
-                }
+                // let val = self.threshold.load(std::sync::atomic::Ordering::Acquire);
+                // let val = val as f32 / 1_000_000.0_f32;
+                let val = 1.0_f32;
+                format!("{}", val * 100.0)
             }
             _ => "".to_string(),
         }
@@ -81,11 +86,10 @@ impl PluginParameters for SimplePluginParameters {
     fn get_parameter(&self, index: i32) -> f32 {
         match index {
             0 => {
-                if let Ok(val) = self.threshold.lock() {
-                    *val
-                }else{
-                    0.0
-                }
+                //let val = self.threshold.load(std::sync::atomic::Ordering::Acquire);
+                //let val = val as f32 / 1_000_000.0_f32;
+                //val
+                1.0_f32
             },
             _ => 0.0,
         }
@@ -96,9 +100,8 @@ impl PluginParameters for SimplePluginParameters {
         match index {
             // We don't want to divide by zero, so we'll clamp the value
             0 => {
-                if let Ok(mut val) = self.threshold.lock() {
-                    *val = value.max(0.01);
-                }
+                //let val = (value * 1_000_000.0_f32) as u32;
+                //self.threshold.store(val, std::sync::atomic::Ordering::Acquire);
             },
             _ => (),
         }
@@ -205,27 +208,99 @@ impl Plugin for BasicPlugin {
         }
     }
 
-    fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
-        let volume = if let Ok(val) = self.params.threshold.lock(){
-            *val
-        }else{
-            0.0_f32
-        };
+    fn process(&mut self, buffer: &mut AudioBuffer<f32>){
+        //let val = self.params.threshold.load(std::sync::atomic::Ordering::Acquire);
+        //let threshold = val as f32 / 1_000_000.0_f32;
+        let threshold = 0.9_f32;
 
-        // split выдает нам пару значений входного и выходного буффера
-        // Сейчас нам нужен толкьо выход, вход игнорируем
-        let (_, mut output_buffer) = buffer.split();
-    
-        // Теперь итерируемся по значениям выходного буффера и заполняем их
-        // Итерация сначала происходит по каналам, затем по данным
-        for output_channel in output_buffer.into_iter() {
-            // Итерируемся по данным канала
-            for output_sample in output_channel {
-                // Выдавать значения надо в диапазоне от -1.0 до 1.0
-                *output_sample = (random::<f32>() - 0.5f32) * 2f32 * volume;
+        // Создаем итератор по парам элементов, входа и выхода
+        for (input, output) in buffer.zip() {
+            let const_val: f32 = input
+                .iter()
+                .fold(1.0_f32, |prev, new|{
+                    if new.abs() < prev.abs(){
+                        *new
+                    }else{
+                        prev
+                    }
+                });
+                /*.map(|val| val.abs())
+                .min_by(|val1, val2|{
+                    if val1 < val2 {
+                        std::cmp::Ordering::Less
+                    }else if val1 > val2{
+                        std::cmp::Ordering::Greater
+                    }else{
+                        std::cmp::Ordering::Equal
+                    }
+                })
+                .unwrap_or(0.0_f32);*/
+
+            // FFTplanner позволяет выбирать оптимальный алгоритм работы для входного размера данных
+            let mut planner = FFTplanner::new(false);
+
+            // Создаем объект, который содержит в себе оптимальный алгоритм преобразования фурье
+            let fft = planner.plan_fft(input.len());
+
+            let mut input_fft: Vec<Complex32> = input
+                .iter()
+                .map(|val|{
+                    Complex32::new(*val, 0.0)
+                    //Complex32::new(*val - const_val, 0.0)
+                    /*if *val < 0.0_f32{
+                        Complex32::new(*val - const_val.abs(), 0.0)
+                        // Complex32::new(*val, 0.0)
+                    }else{
+                        Complex32::new(*val - const_val.abs(), 0.0)
+                        // Complex32::new(*val, 0.0)
+                    }*/
+                })
+                .collect();
+
+            let mut output_fft: Vec<Complex32> = vec![Complex::zero(); output.len()];
+
+            // Обрабатываем данные
+            // Входные данные мутабельные, так как они используются в качестве буффера
+            // Как результат - там будет мусор после вычисления
+            fft.process(&mut input_fft, &mut output_fft);
+            
+            let sqrt_len = 1.0 / (output_fft.len() as f32).sqrt();
+            output_fft
+                .iter_mut()
+                .for_each(|val|{
+                    *val *= sqrt_len;
+                });
+
+            fft.process(&mut output_fft, &mut input_fft);
+
+            input_fft
+                .iter_mut()
+                .for_each(|val|{
+                    *val *= sqrt_len;
+                });
+
+
+            // Для каждого входного и выходного семпла в буфферах
+            for (in_sample, out_sample) in input_fft.into_iter().zip(output.into_iter()) {
+                /*let val = if in_sample.re < 0.0_f32{
+                    in_sample.re + min_abs
+                }else{
+                    in_sample.re + min_abs
+                };*/
+                let val = in_sample.re;
+                // let val = in_sample.re + const_val;
+
+                // Эмулируем клиппинг значений
+                *out_sample = if val > threshold {
+                    threshold
+                } else if val < -threshold {
+                    -threshold
+                } else {
+                    val
+                };
             }
         }
-    } 
+    }
 
     // Выдаем ссылку на шареный объект параметров
     fn get_parameter_object(&mut self) -> Arc<dyn PluginParameters> {
