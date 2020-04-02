ea1e79082d8bafea1bbe456a323264b7f223e3e3
DevNul <devnulpavel@gmail.com>
2020-04-02 21:58:22 +0300

    Refactoring

--- test40_gRPC/src/bin/client_route.rs
+++ test40_gRPC/src/bin/client_route.rs
@@ -1,21 +1,31 @@
+pub mod routeguide {
+    tonic::include_proto!("route_guide");
+}
+
 use std::error::Error;
 use std::time::Duration;
-
 use futures::stream;
 use rand::rngs::ThreadRng;
 use rand::Rng;
 use tokio::time;
 use tonic::transport::Channel;
 use tonic::Request;
-
 use routeguide::route_guide_client::RouteGuideClient;
 use routeguide::{Point, Rectangle, RouteNote};
 
-pub mod routeguide {
-    tonic::include_proto!("route_guide");
+
+fn random_point(rng: &mut ThreadRng) -> Point {
+    let latitude = (rng.gen_range(0, 180) - 90) * 10_000_000;
+    let longitude = (rng.gen_range(0, 360) - 180) * 10_000_000;
+    Point {
+        latitude,
+        longitude,
+    }
 }
 
+// Запрос данных с сервера
 async fn print_features(client: &mut RouteGuideClient<Channel>) -> Result<(), Box<dyn Error>> {
+    // Местность, где определяем
     let rectangle = Rectangle {
         lo: Some(Point {
             latitude: 400_000_000,
@@ -27,11 +37,13 @@ async fn print_features(client: &mut RouteGuideClient<Channel>) -> Result<(), Bo
         }),
     };
 
+    // Вызываем получение данных у сервера в виде потока
     let mut stream = client
         .list_features(Request::new(rectangle))
         .await?
         .into_inner();
 
+    // Обрабатываем новые данные от сервера
     while let Some(feature) = stream.message().await? {
         println!("NOTE = {:?}", feature);
     }
@@ -62,7 +74,7 @@ async fn run_record_route(client: &mut RouteGuideClient<Channel>) -> Result<(), 
 async fn run_route_chat(client: &mut RouteGuideClient<Channel>) -> Result<(), Box<dyn Error>> {
     let start = time::Instant::now();
 
-    let outbound = async_stream::stream! {
+    let outbound = async_stream::stream!{
         let mut interval = time::interval(Duration::from_secs(1));
 
         while let time = interval.tick().await {
@@ -91,8 +103,10 @@ async fn run_route_chat(client: &mut RouteGuideClient<Channel>) -> Result<(), Bo
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Создаем клиента
     let mut client = RouteGuideClient::connect("http://127.0.0.1:10000").await?; // "http://[::1]:10000"
 
+    // Запрашиваем с сервера особенности в данной точке
     println!("*** SIMPLE RPC ***");
     let response = client
         .get_feature(Request::new(Point {
@@ -102,23 +116,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
     println!("RESPONSE = {:?}", response);
 
+    // Запрашиваем у сервера список всех особенностей местности
     println!("\n*** SERVER STREAMING ***");
     print_features(&mut client).await?;
 
+    // Закидываем на сервера наши данные потоком
     println!("\n*** CLIENT STREAMING ***");
     run_record_route(&mut client).await?;
 
+    // Двунаправленный поток
     println!("\n*** BIDIRECTIONAL STREAMING ***");
     run_route_chat(&mut client).await?;
 
     Ok(())
-}
-
-fn random_point(rng: &mut ThreadRng) -> Point {
-    let latitude = (rng.gen_range(0, 180) - 90) * 10_000_000;
-    let longitude = (rng.gen_range(0, 360) - 180) * 10_000_000;
-    Point {
-        latitude,
-        longitude,
-    }
 }
