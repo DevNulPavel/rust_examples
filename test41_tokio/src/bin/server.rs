99d6a2d7b2f5485e280160338c31062d8ce98578
DevNul <devnulpavel@gmail.com>
2020-04-04 11:52:46 +0300

    Some improvements in Tokio

--- test41_tokio/src/bin/server.rs
+++ test41_tokio/src/bin/server.rs
@@ -1,14 +1,27 @@
 #[allow(unused_imports)]
 
 use std::io::Cursor;
+use std::process::Output;
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
 use tokio::prelude::*;
 use tokio::runtime::Handle;
+use tokio::process::{ Command, Child };
+use tokio::sync::{ Mutex };
+use tokio::sync::mpsc::{channel, unbounded_channel, Receiver, Sender, UnboundedReceiver, UnboundedSender};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{ TcpListener, TcpStream};
 use tokio::net::tcp::{ Incoming, ReadHalf, WriteHalf };
 use futures::stream::StreamExt;
 use bytes::{Bytes, BytesMut, Buf, BufMut};
 use byteorder::{ReadBytesExt, WriteBytesExt, BigEndian, LittleEndian};
 use serde::Deserialize;
+use test41_tokio::{EmptyResult, StringResult, save_file_from_socket};
+
+type PathSender = UnboundedSender<(PathBuf, ResultSender)>;
+type PathReceiver = UnboundedReceiver<(PathBuf, ResultSender)>;
+type ResultSender = UnboundedSender<(PathBuf, String)>;
+type ResultReceiver = UnboundedReceiver<(PathBuf, String)>;
 
 struct ServerStatus{
     active_converts: u16,
@@ -19,82 +32,95 @@ struct ServerStatus{
 #[derive(Deserialize)]
 struct ReceivedMeta{
     file_size: usize,
-    filename: String,
+    file_name: String,
 }
 
-async fn process_sending_data<'a>(mut writer: WriteHalf<'a>) {
-}
-
-async fn save_file_from_socket<'a>(reader: &mut ReadHalf<'a>, info: ReceivedMeta){
-    let mut file: tokio::fs::File = tokio::fs::File::create(info.filename)
-        .await
-        .unwrap();
-    
-    let mut data_buffer: [u8; 1024] = [0; 1024];
-    let mut size_left = info.file_size;
-
-    while size_left > 0 {
-        let read_size = data_buffer.len().min(size_left);
-        let buffer_slice = &mut data_buffer[0..read_size];
-        let read_result = reader.read_exact(buffer_slice).await;
-        match read_result {
-            Ok(size) => {
-                let result_slice = &mut data_buffer[0..size];
-                file.write(result_slice)
-                    .await
-                    .unwrap();
-                size_left -= size;
-            },
-            Err(e) => {
-                eprintln!("Read from socket error: {}", e);
-                break;
-            }
+async fn get_md5(path: &Path) -> StringResult {
+    let path_str = match path.to_str(){
+        Some(path_str) => {
+            path_str
+        },
+        None => {
+            return Err("Empty path".into());
         }
+    };
+
+    let child = Command::new("md5")
+        .arg(path_str)
+        .output();
+
+    let out: Output = child.await?;    
+
+    println!("the command exited with: {:?}", out);
+
+    if out.status.success() {
+        let text = String::from_utf8(out.stdout)?;
+        Ok(text)
+    }else{
+        let text = String::from_utf8(out.stderr)?;
+        Err(text.into())
     }
 }
 
-async fn process_incoming_data<'a>(mut reader: ReadHalf<'a>) {
+async fn process_files(mut receiver: PathReceiver)-> EmptyResult{
+    // TODO: Ограничение на количество одновременных обработок
+    //let (input_sender, input_receiver) = channel::<PathBuf>(8);
+    //let (output_sender, output_receiver) = channel::<(PathBuf, String)>(8);
     loop {
-        let mut data_size_buffer: [u8; 4] = [0; 4];
-        let read_result = reader.read_exact(&mut data_size_buffer).await;
-        match read_result {
-            Ok(size) if size == data_size_buffer.len() => {
-                let json_size = u32::from_le_bytes(data_size_buffer) as usize;
-                let mut buffer: BytesMut = BytesMut::with_capacity(json_size);
-                match reader.read_exact(&mut buffer).await {
-                    Ok(_) => {
-                        let json: ReceivedMeta = serde_json::from_slice(&buffer).unwrap();
-                        save_file_from_socket(&mut reader, json).await;
-                    },
-                    Err(err) => {
-                        eprintln!("Read from socket error: {}", err);
-                        break;
-                    }
-                }
+        let (received_path, response_ch) = match receiver.recv().await{
+            Some(data) => {
+                data
             },
-            Ok(size) => {
-                eprintln!("Read from socket: size == {}", size);
-                break;
-            },
-            Err(err) => {
-                eprintln!("Read from socket error: {}", err);
+            None => {
                 break;
             }
+        };
+
+        if let Ok(md5_res) = get_md5(&received_path).await {
+            response_ch.send((received_path, md5_res))?;
+        }
+    }
+
+    Ok(())
+}
+
+async fn process_sending_data<'a>(mut writer: WriteHalf<'a>, mut receiver: ResultReceiver) {
+}
+
+
+async fn process_incoming_data<'a>(mut reader: ReadHalf<'a>, process_sender: PathSender, sock_channel: ResultSender) -> EmptyResult {
+    loop {
+        let json_size: usize = reader.read_u16().await? as usize;
+
+        let mut buffer: BytesMut = BytesMut::with_capacity(json_size);
+        reader.read_exact(&mut buffer).await?;
+
+        let json: ReceivedMeta = serde_json::from_slice(&buffer).unwrap();
+
+        let file_path = PathBuf::new()
+            .join(json.file_name);
+        let save_result = save_file_from_socket(&mut reader, json.file_size, &file_path).await;
+        if save_result.is_ok() {
+            process_sender.send((file_path, sock_channel.clone()))?;
         }
     }
 }
 
-async fn process_connection(mut sock: TcpStream){
+async fn process_connection(mut sock: TcpStream, process_sender: PathSender){
     //let sock = std::sync::Arc::from(sock);
     // Получаем отдельные каналы сокета на чтение и на запись
     let (reader, writer) = sock.split();
     let reader: ReadHalf = reader;
     let writer: WriteHalf = writer;
-    
-    let reader_join = process_incoming_data(reader);
-    let writer_join = process_sending_data(writer);
 
-    tokio::join!(reader_join, writer_join);
+    let (sender, receiver) = unbounded_channel::<(PathBuf, String)>();
+    
+    let reader_join = process_incoming_data(reader, process_sender, sender);
+    let writer_join = process_sending_data(writer, receiver);
+
+    let _ = reader_join.await;
+    let _ = writer_join.await;
+    // tokio::join!(reader_join, writer_join);
 
     /*let copy_result = tokio::io::copy(&mut reader, &mut writer).await;
     match copy_result {
@@ -107,8 +133,18 @@ async fn process_connection(mut sock: TcpStream){
     }*/
 }
 
+fn create_processing() -> (impl futures::Future<Output = EmptyResult>, PathSender) {
+    let (processing_sender, input_receiver) = unbounded_channel::<(PathBuf, ResultSender)>();
+    
+    let process_file_future = process_files(input_receiver);
+
+    (process_file_future, processing_sender)
+}
+
 #[tokio::main]
 async fn main() {
+    let (process_file_future, processing_sender) = create_processing();
+
     let addr = "127.0.0.1:6142";
 
     // Дожидаемся успешного создания серверного сокета
@@ -116,6 +152,9 @@ async fn main() {
         .await
         .unwrap();
     
+    // TODO: !!! убрать, либо чистить с проверкой результата
+    let mut active_futures = vec![];
+
     // Создаем обработчик
     let server = async move {
         // Получаем поток новых соединений
@@ -127,7 +166,12 @@ async fn main() {
                 Ok(sock) => {
                     // Закидываем задачу по работе с нашим сокетом в отдельную функцию
                     // tokio::spawn - неблокирующая функция, закидывающая задачу в обработку
-                    tokio::spawn(process_connection(sock));
+                    let sender = processing_sender.clone();
+                    let fut = process_connection(sock, sender);
+                    active_futures.push(fut);
+
+                    // Не работает, так как фьючи могут пережить каналы и обработку данных
+                    //tokio::spawn(fut);
                 }
                 Err(e) => {
                     eprintln!("accept failed = {:?}", e)
@@ -140,6 +184,8 @@ async fn main() {
     
     // Блокируемся на выполнении сервера
     server.await;
+
+    process_file_future.await;
 
     // TODO: завершение по Ctrl + C
 }
--- test41_tokio/src/lib.rs
+++ test41_tokio/src/lib.rs
@@ -16,3 +16,72 @@
 // другие файлы .rs лучше воспринимать как папку с корневым файлом mod.rs
 // https://users.rust-lang.org/t/importing-module-from-another-module/18172/9
 
+
+use std::io::Cursor;
+use std::path::Path;
+use tokio::prelude::*;
+use tokio::runtime::Handle;
+use tokio::net::{ TcpListener, TcpStream};
+use tokio::net::tcp::{ Incoming, ReadHalf, WriteHalf };
+use futures::stream::StreamExt;
+use bytes::{Bytes, BytesMut, Buf, BufMut};
+use byteorder::{ReadBytesExt, WriteBytesExt, BigEndian, LittleEndian};
+use serde::Deserialize;
+
+pub type EmptyResult = Result<(), Box<dyn std::error::Error>>;
+pub type StringResult = Result<String, Box<dyn std::error::Error>>;
+
+pub async fn save_file_from_socket<'a>(reader: &mut ReadHalf<'a>, file_size: usize, path: &Path) -> EmptyResult {
+    if file_size == 0{
+        panic!("File size can't be zero: {:?}", path);
+    }
+
+    // Создаем асинхронно файлик
+    let mut file: tokio::fs::File = tokio::fs::File::create(path)
+        .await?;
+    
+    let mut data_buffer: [u8; 1024] = [0; 1024];
+    let mut size_left = file_size;
+
+    while size_left > 0 {
+        // Сколько чатаем из сокета
+        let read_size = data_buffer.len().min(size_left);
+        
+        // Слайс нужного размера
+        let buffer_slice = &mut data_buffer[0..read_size];
+
+        // Читаем из файлика
+        let read_size_result = match reader.read_exact(buffer_slice).await {
+            Ok(read_size) => {
+                read_size
+            },
+            Err(err) => {
+                tokio::fs::remove_file(path).await?;
+                return Err(err.into());
+            }   
+        };
+
+        // Проверяем, что прочитали норм все
+        if read_size_result == read_size {
+            // Слайс прочитанных данных
+            let result_slice = &mut data_buffer[0..read_size_result];
+
+            // Пишем в файлик
+            if let Err(e) = file.write_all(result_slice).await {
+                tokio::fs::remove_file(path).await?;
+                return Err(e.into());
+            }
+            
+            // Уменьшаем оставшееся количество байт на чтение
+            size_left -= read_size_result;
+        }else{
+            break;
+        }
+    }
+
+    if size_left == 0 {
+        Ok(())
+    }else{
+        return Err(format!("Read from socet failed: {:?}", path).into());
+    }
+}
