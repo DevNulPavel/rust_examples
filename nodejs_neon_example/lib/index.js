552290271bd8a4ad094b87a0cdc41312c48bc6b8
Pavel Ershov <pershov@game-insight.com>
2020-06-03 16:14:17 +0300

    New examples

--- nodejs_neon_example/lib/index.js
+++ nodejs_neon_example/lib/index.js
@@ -1,4 +1,5 @@
-var addon = require('../native');
+const addon = require('../native');
+
 
 function main(){
     let text = addon.hello();
@@ -13,11 +14,59 @@ function main(){
     let args_len = addon.get_args_len("test", 123);
     console.log(`Args count: ${args_len}`);
 
+    //////////////////////////////////////////////////////////////////////////////////////////
+
     let object = addon.test_objects();
     let object_func = object.function_property;
     let func_result = object_func();
     console.log(`Object: ${object}`);
     console.log(`Function: ${object_func}, returnned from function "${func_result}"`);
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+
+    let modify_func = addon.modify_object_this; // Копируем функцию, чтобы this использовался из контекста, а не из addon
+    let func1 = function (){
+        // this - это контекст данной функции
+        //this.modified = false;
+        modify_func();
+        console.log(`This modify (func): ${this.modified}`);
+    };
+    func1();
+    let func2 = () => {
+        // this - это контекст функции main
+        //this.modified = false;
+        modify_func();
+        console.log(`This modify (lambda): ${this.modified}`);
+    };
+    func2();
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+
+    addon.function_as_parameter((param)=>{
+        console.log(`Js function called from Rust with param: ${param}`);
+        return 10;
+    });
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+
+    //const date = new Date('December 31, 1975, 23:15:30 GMT+11:00');
+    let year = addon.construct_js_function(Date);
+    console.log(`Received year: ${year}`);
+    // construct_obj.getUTCFullYear = (param)=>{
+    //     console.log(`Js function called from Rust with param: ${param}`);
+    // };
+    //construct_obj(12);
+    /*addon.construct_js_function((param)=>{
+        console.log(`Js function called from Rust with param: ${param}`);
+        return 10;
+    })*/
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+
+    const john = new addon.Employee(10, "John");
+    john.name(); // John
+    john.greet(); // Hi John!
+    john.askQuestion(); // How are you?
 }
 
 main();
--- nodejs_neon_example/native/src/lib.rs
+++ nodejs_neon_example/native/src/lib.rs
@@ -1,6 +1,10 @@
 use neon::{
     prelude::{
         *
+    },
+    borrow::{
+        Ref,
+        //RefMut
     }
 };
 
@@ -68,9 +72,108 @@ fn test_objects(mut cx: FunctionContext) -> JsResult<JsObject>{
     Ok(js_object)
 }
 
+pub fn modify_object_this(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+    // Получаем наш объект this, контекст функции
+    // Обычные функции имеют свой контекст, лямбды захватывают контекст this вызвавшей функции
+    let this = cx.this();
+    
+    // Приводим this к типу JsObject, чтобы можно было вызвать .set
+    let this = this.downcast::<JsObject>().or_throw(&mut cx)?;
+
+    // Создаем переменную bool
+    let t = cx.boolean(true);
+    
+    // Устанавливаем значение переменной modified у объекта, аналогично "this.modified = true"
+    this.set(&mut cx, "modified", t)?;
+
+    Ok(cx.undefined())
+}
+
+pub fn function_as_parameter(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+    // Получаем нашу переданную функцию
+    let func = cx.argument::<JsFunction>(0)?;
+
+    // Создаем массив аргуметов нашей функции
+    /*let args: [Handle<JsNumber>; 1] = [cx.number(16.0)];
+    let iter = args
+        .into_iter()
+        .map(|val| { 
+            &val 
+        });*/
+
+    // Создаем владеющий вектор аргуметов нашей функции
+    let args: Vec<Handle<JsNumber>> = vec![cx.number(16.0)];
+    
+    // Контекст this будет null
+    let null_this = cx.null();
+
+    // Вызываем нашу JS функцию с параметрами
+    let received_number = func
+        .call(&mut cx, null_this, args)?
+        .downcast::<JsNumber>()
+        .or_throw(&mut cx)?
+        .value();
+    
+    println!("RUST: value {}", received_number);
+
+    Ok(cx.undefined())
+}
+
+// TODO: Test it
+/// Мы можем создать JS функцию с контекстом
+pub fn construct_js_function(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    // Получаем функцию как параметр, передаем функцию Date, которая ведет себя как класс
+    let param_js_func = cx.argument::<JsFunction>(0)?;
+
+    // Нулевое значение
+    //let zero = cx.number(0.0);
+    let text = cx.string("December 31, 1975, 23:15:30 GMT+11:00");
+
+    // Создаем новую функцию, но уже с замыканием определенных значений?
+    // Аналогично: const date = new Date('December 31, 1975, 23:15:30 GMT+11:00');
+    let date_obj = param_js_func.construct(&mut cx, vec![text])?;
+
+    // Получаем метод getUTCFullYear у нового объекта-функции
+    let get_utc_full_year_method = date_obj
+        .get(&mut cx, "getUTCFullYear")? // Получаем функцию getUTCFullYear у объекта
+        .downcast::<JsFunction>()
+        .or_throw(&mut cx)?;
+
+    // Аргументы
+    let args: Vec<Handle<JsValue>> = vec![];
+    let out_js_val = date_obj.upcast::<JsValue>();
+
+    // Вызываем данный метод
+    get_utc_full_year_method
+        .call(&mut cx, out_js_val, args)?
+        .downcast::<JsNumber>()
+        .or_throw(&mut cx)
+}
+
+pub struct Employee {
+    // Переменные структуры мапятся в свойства класса JS 
+    _id: i32,
+    name: String
+}
+
+impl Employee {
+    pub fn new(id: i32, name: String) -> Self { 
+        Self{ 
+            _id: id, 
+            name 
+        } 
+    }
+
+    /*fn talk(mut cx: FunctionContext) -> JsResult<JsValue> {
+        let obj = cx.string("How are you doing?")
+            .upcast();
+        Ok(obj)
+    }*/
+}
+
 // Asynchronously compute fibonacci on another thread
-fn fibonacci_async(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    /*// Получаем аргумент 0
+/*fn fibonacci_async(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+    // Получаем аргумент 0
     let n = cx.argument::<JsNumber>(0)?.value() as usize;
     // Получаем аргумент 1 - коллбек
     let cb = cx.argument::<JsFunction>(1)?;
@@ -78,9 +181,70 @@ fn fibonacci_async(mut cx: FunctionContext) -> JsResult<JsUndefined> {
     let task = FibonacciTask{ 
         argument: n 
     };
-    task.schedule(cb);*/
+    task.schedule(cb);
     
     Ok(cx.undefined())
+}*/
+
+declare_types! {
+    /// Описываем класс, который оборачивает Employee записи
+    pub class JsEmployee for Employee {
+        // Метод-конструктор JS
+        init(mut cx) {
+            // Получаем id
+            let id = cx.argument::<JsNumber>(0)?
+                .value();
+            
+            // Получаем имя
+            let name: String = cx
+                .argument::<JsString>(1)?
+                .value();
+
+            let e = Employee::new(id as i32, name);
+
+            Ok(e)
+        }
+
+        method talk(mut cx) {
+            Ok(cx.string("How are you doing?").upcast())
+        }
+
+        method name(mut cx) {
+            // Получаем текущий объект
+            let this = cx.this();
+            // Получаем имя
+            let name: String = {
+                // Получаем блокировку контекста
+                let guard = cx.lock();
+                // Получаем мутабельную ссылку на наш объект
+                let rust_obj: Ref<&mut Employee> = this.borrow(&guard);
+                // Создаем копию имени
+                rust_obj.name.clone()
+            };
+            println!("RUST: {}", &name);
+            Ok(cx.undefined().upcast())
+        }
+
+        method greet(mut cx) {
+            // Получаем текущий объект
+            let this = cx.this();
+            let msg: String = {
+                // Получаем блокировку контекста
+                let guard = cx.lock();
+                // Получаем мутабельную ссылку на наш объект
+                let greeter: Ref<&mut Employee> = this.borrow(&guard);
+                // Форматируем строку
+                format!("RUST: Hi {}!", greeter.name)
+            };
+            println!("{}", &msg);
+            Ok(cx.string(&msg).upcast())
+        }
+
+        method askQuestion(mut cx) {
+            println!("RUST: {}", "How are you?");
+            Ok(cx.undefined().upcast())
+        }
+    }
 }
 
 // Экспортируем наши Rust функции
@@ -90,5 +254,13 @@ register_module!(mut cx, {
     cx.export_function("make_an_array", make_an_array)?;
     cx.export_function("get_args_len", get_args_len)?;
     cx.export_function("test_objects", test_objects)?;
+    cx.export_function("modify_object_this", modify_object_this)?;
+    cx.export_function("function_as_parameter", function_as_parameter)?;
+    cx.export_function("construct_js_function", construct_js_function)?;
+    
+    // JsEmployee враппер говорит neon, какой именно класс мы экспортируем
+    // "Employee" - это имя класса в JS
+    cx.export_class::<JsEmployee>("Employee")?;
+
     Ok(())
 });
