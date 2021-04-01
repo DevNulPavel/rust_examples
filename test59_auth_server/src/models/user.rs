0ac455047ddc0530c3d7f17b4ecd289f8bcb1875
Pavel Ershov <devnulpavel@gmail.com>
2021-04-01 13:12:34 +0300

    Layout improvements

--- test59_auth_server/src/models/user.rs
+++ test59_auth_server/src/models/user.rs
@@ -4,8 +4,16 @@ use uuid::{
 use chrono::{
     NaiveDateTime
 };
+use serde::{
+    Serialize,
+    Deserialize
+};
+use validator_derive::{
+    Validate
+};
 
-pub struct User{
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
     pub uuid: Uuid,
     pub user_name: String,
     pub email: String,
@@ -16,4 +24,22 @@ pub struct User{
     pub user_image: Option<String>,
     pub create_at: NaiveDateTime,
     pub updated_at: NaiveDateTime
+}
+
+#[derive(Debug, Serialize, Deserialize, Validate)]
+pub struct NewUser {
+    #[validate(length(min = 3))]
+    pub user_name: String,
+    #[validate(email)]
+    pub email: String,
+    #[validate(length(min = 3))]
+    pub password: String
+}
+
+#[derive(Debug, Serialize, Deserialize, Validate)]
+pub struct UpdateProfile {
+    pub full_name: Option<String>,
+    pub bio: Option<String>,
+    #[validate(url)]
+    pub image: Option<String>
 }
