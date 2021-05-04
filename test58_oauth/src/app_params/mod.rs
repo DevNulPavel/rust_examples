9a04235ee031e05d3b7bc9629bb121b0fed5d41f
Pavel Ershov <devnulpavel@gmail.com>
2021-05-04 13:12:14 +0300

    Old example removed

--- test58_oauth/src/app_params/mod.rs
+++
@@ -1,9 +0,0 @@
-mod env_app_params;
-
-pub use self::{
-    env_app_params::{
-        FacebookEnvParams,
-        GoogleEnvParams,
-        AppEnvParams
-    }
-};
--- test58_oauth/src/constants/mod.rs
+++
@@ -1,9 +0,0 @@
-pub const INDEX_TEMPLATE: &str = "index";
-pub const LOGIN_TEMPLATE: &str = "login";
-
-pub const INDEX_PATH: &str = "/";
-pub const LOGIN_PATH: &str = "/login";
-pub const LOGOUT_PATH: &str = "/logout";
-pub const AUTH_CALLBACK_PATH: &str = "/auth_callback";
-pub const FACEBOOK_SCOPE_PATH: &str = "/facebook";
-pub const GOOGLE_SCOPE_PATH: &str = "/google";
