5472518514bc67cf3a7d2fd750540dc1616d5b41
Pavel Ershov <devnulpavel@gmail.com>
2021-05-06 09:41:07 +0300

    New examples

--- test62_wasm/src/lib.rs
+++ test62_wasm/src/lib.rs
@@ -34,6 +34,42 @@ extern {
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        #[allow(unused_unsafe)]
+        unsafe{
+            web_sys::console::log_1(&format!( $( $t )* ).into());
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        #[allow(unused_unsafe)]
+        unsafe{
+            web_sys::console::time_with_label(name);
+        }
+        Timer { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        #[allow(unused_unsafe)]
+        unsafe{
+            web_sys::console::time_end_with_label(self.name);
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
 #[wasm_bindgen]
 pub fn greet(text: &str) {
     set_panic_hook();
@@ -54,6 +90,27 @@ pub enum Cell {
     Alive = 1,
 }
 
+impl Cell {
+    fn random_cell() -> Cell {
+        #[allow(unused_unsafe)]
+        let val = unsafe{
+            random() < 0.5
+        };
+        if val {
+            Cell::Alive
+        } else {
+            Cell::Dead
+        }
+    }
+
+    fn toggle(&mut self) {
+        *self = match *self {
+            Cell::Dead => Cell::Alive,
+            Cell::Alive => Cell::Dead,
+        };
+    }
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 #[wasm_bindgen]
@@ -67,6 +124,8 @@ pub struct Universe {
 #[wasm_bindgen]
 impl Universe {
     pub fn new() -> Universe {
+        utils::set_panic_hook();
+
         let width = 128;
         let height = 128;
 
@@ -74,27 +133,17 @@ impl Universe {
 
         let cells = (0..width * height)
             .map(|_| {
-                #[allow(unused_unsafe)]
-                let val = unsafe{
-                    random() < 0.5
-                };
-                if val {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
+                Cell::random_cell()
             })
             .collect();
+
+        log!("Universe created");
 
         Universe {
             width,
             height,
             cells,
         }
-    }
-
-    fn get_index(&self, row: u32, column: u32) -> usize {
-        (row * self.width + column) as usize
     }
 
     pub fn get_width(&self) -> u32 {
@@ -109,28 +158,13 @@ impl Universe {
         self.cells.as_ptr()
     }
 
-    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-        let mut count = 0;
-        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-                if delta_row == 0 && delta_col == 0 {
-                    continue;
-                }
-
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col = (column + delta_col) % self.width;
-                let idx = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[idx] as u8;
-            }
-        }
-        count
-    }
-
     pub fn render(&self) -> String {
         self.to_string()
     }
 
     pub fn tick(&mut self) {
+        let _timer = Timer::new("Universe::tick");
+
         let mut next = self.cells.clone();
 
         for row in 0..self.height {
@@ -175,6 +209,77 @@ impl Universe {
         }
 
         self.cells = next;
+    }
+
+    pub fn toggle_cell(&mut self, row: u32, column: u32) {
+        let idx = self.get_index(row, column);
+        self.cells[idx].toggle();
+    }
+
+    pub fn reset_game(&mut self) {
+        self
+            .cells
+            .iter_mut()
+            .for_each(|val|{
+                *val = Cell::random_cell();
+            })
+    }
+}
+
+impl Universe{
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    /// Set the width of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height)
+            .map(|_i| Cell::Dead)
+            .collect();
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height)
+            .map(|_i| Cell::Dead)
+            .collect();
+    }
+
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
     }
 }
 
