3bc7cfcb1fae27cbd19d59678a17df64d3dca7be
Pavel Ershov <devnulpavel@gmail.com>
2021-05-25 18:31:37 +0300

    Dynamo logic improvements

--- test65_dynamodb/src/dynamodb.rs
+++ test65_dynamodb/src/dynamodb.rs
@@ -1,9 +1,9 @@
-use dynomite::{retry::Policy, Attributes, DynamoDbExt, Item, Retries};
+use dynomite::{attr_map, retry::Policy, Attribute, Attributes, DynamoDbExt, FromAttributes, Item, Retries};
 use eyre::WrapErr;
 use rusoto_core::{Region, RusotoError};
 use rusoto_dynamodb::{
-    AttributeDefinition, CreateTableError, CreateTableInput, DynamoDb, DynamoDbClient, KeySchemaElement,
-    ListTablesInput, ProvisionedThroughput, PutItemInput,
+    AttributeDefinition, CreateTableError, CreateTableInput, DescribeTableError, DescribeTableInput, DynamoDb,
+    DynamoDbClient, GetItemInput, KeySchemaElement, ListTablesInput, ProvisionedThroughput, PutItemInput,
 };
 use tracing::{debug, error, info, instrument, warn};
 use uuid::Uuid;
@@ -29,9 +29,15 @@ pub struct Book {
     // Уникальный идентификатор
     #[dynomite(partition_key)]
     id: Uuid,
+
+    // Вторичный ключ сортировки
+    #[dynomite(sort_key)]
+    year: u32,
+
     // Имя книги
     #[dynomite(rename = "book_title")]
     title: String,
+
     // Список авторов
     authors: Option<Vec<Author>>,
 }
@@ -68,15 +74,30 @@ async fn print_tables_list(client: &impl DynamoDb) -> Result<(), eyre::Error> {
 async fn create_table_if_needed(client: &impl DynamoDb, table_name: String) -> Result<(), eyre::Error> {
     // Parameters
     let input = CreateTableInput {
-        table_name,
-        key_schema: vec![KeySchemaElement {
-            attribute_name: "id".to_string(),
-            key_type: "HASH".to_string(),
-        }],
-        attribute_definitions: vec![AttributeDefinition {
-            attribute_name: "id".to_string(),
-            attribute_type: "S".to_string(),
-        }],
+        table_name: table_name.clone(),
+        // Описание ключей
+        key_schema: vec![
+            KeySchemaElement {
+                attribute_name: "id".to_string(),
+                key_type: "HASH".to_string(), // Первичный ключ - HASH, ключ сортировки - RANGE
+            },
+            KeySchemaElement {
+                attribute_name: "year".to_string(),
+                key_type: "RANGE".to_string(), // Первичный ключ - HASH, ключ сортировки - RANGE
+            },
+        ],
+        // Описание типов ключей
+        attribute_definitions: vec![
+            AttributeDefinition {
+                attribute_name: "id".to_string(),
+                attribute_type: "S".to_string(),
+            },
+            AttributeDefinition {
+                attribute_name: "year".to_string(),
+                attribute_type: "N".to_string(), // [B, N, S]
+            },
+        ],
+        // Количество записей и чтения на отдельную таблицу в секунду
         provisioned_throughput: Some(ProvisionedThroughput {
             read_capacity_units: 1,
             write_capacity_units: 1,
@@ -90,7 +111,8 @@ async fn create_table_if_needed(client: &impl DynamoDb, table_name: String) -> R
     // Смотрим результат
     match res {
         Ok(result) => {
-            info!(?result, "Table created");
+            info!("Table created");
+            debug!(table_create_result = ?result);
         }
         Err(RusotoError::Service(CreateTableError::ResourceInUse(err))) => {
             debug!(%err, "Table already exists, creating is not required");
@@ -100,12 +122,49 @@ async fn create_table_if_needed(client: &impl DynamoDb, table_name: String) -> R
         }
     }
 
+    // Дожидаемся когда таблица станет доступна для работы
+    let mut check_count = 0;
+    loop {
+        // Получаем информацию о таблице
+        let status = client
+            .describe_table(DescribeTableInput {
+                table_name: table_name.clone(),
+                ..Default::default()
+            })
+            .await?
+            .table
+            .ok_or_else(||{
+                eyre::eyre!("Table info request failed")
+            })?
+            .table_status
+            .ok_or_else(||{
+                eyre::eyre!("Table status is missing")
+            })?;
+
+        // Узнаем статус
+        match status.as_str() {
+            "ACTIVE" => {
+                info!("Table is available now");
+                break;                
+            },
+            status @ _ => {
+                if check_count < 5 {
+                    info!(status, "Table will be awailable soon, awaiting");
+                    tokio::time::delay_for(std::time::Duration::from_secs(1)).await;
+                    check_count += 1;
+                }else{
+                    return Err(eyre::eyre!("Table available check timeout, status: {}", status));
+                }
+            }
+        }
+    }
+
     Ok(())
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[instrument(skip(client))]
+#[instrument(skip(client, book), fields(book_id = %book.id))]
 async fn insert_book(client: &impl DynamoDb, table_name: String, book: Book) -> Result<(), eyre::Error> {
     let input = PutItemInput {
         table_name,
@@ -115,7 +174,35 @@ async fn insert_book(client: &impl DynamoDb, table_name: String, book: Book) -> 
 
     let result = client.put_item(input).await?;
 
+    info!(book_result = ?result, "book insert success");
+
     Ok(())
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[instrument(skip(client))]
+async fn get_book_with_title(
+    client: &impl DynamoDb,
+    table_name: String,
+    book_key: Attributes,
+) -> Result<Option<Book>, eyre::Error> {
+    let result = client
+        .get_item(GetItemInput {
+            table_name,
+            key: book_key,
+            ..Default::default()
+        })
+        .await?
+        .item;
+
+    match result {
+        Some(data) => {
+            let book = Book::from_attrs(data)?;
+            Ok(Some(book))
+        }
+        None => Ok(None),
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -129,22 +216,30 @@ pub async fn test_dynamo_db() -> Result<(), eyre::Error> {
     });*/
     let client = DynamoDbClient::new(Region::EuWest2);
 
+    let table_name = "books";
+
     // Выводим таблички
     print_tables_list(&client).await?;
 
     // Создаем табличку
-    create_table_if_needed(&client, "books".to_owned()).await?;
+    create_table_if_needed(&client, table_name.to_owned()).await?;
 
     // Добавляем книжку
     let book = Book {
         id: Uuid::new_v4(),
+        year: 1999,
         title: "first_book_title".to_string(),
         authors: Some(vec![Author {
             id: Uuid::new_v4(),
             name: "Book author title".to_string(),
         }]),
     };
-    insert_book(&client, "books".to_owned(), book).await?;
+    let new_book_key = book.key();
+    insert_book(&client, table_name.to_owned(), book).await?;
+
+    // Пытаемся получить эту книжку
+    let received_book = get_book_with_title(&client, table_name.to_owned(), new_book_key).await?;
+    info!(?received_book, "Book received");
 
     Ok(())
 }
