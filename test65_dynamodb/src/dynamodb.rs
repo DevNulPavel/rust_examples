b726f1318fdb749c96af9b54bb29eeca54e9d3b4
Pavel Ershov <devnulpavel@gmail.com>
2021-05-26 10:10:05 +0300

    New commentaries

--- test65_dynamodb/src/dynamodb.rs
+++ test65_dynamodb/src/dynamodb.rs
@@ -1,10 +1,7 @@
 use dynomite::{attr_map, retry::Policy, Attribute, Attributes, DynamoDbExt, FromAttributes, Item, Retries};
 use eyre::WrapErr;
 use rusoto_core::{Region, RusotoError};
-use rusoto_dynamodb::{
-    AttributeDefinition, CreateTableError, CreateTableInput, DescribeTableError, DescribeTableInput, DynamoDb,
-    DynamoDbClient, GetItemInput, KeySchemaElement, ListTablesInput, ProvisionedThroughput, PutItemInput,
-};
+use rusoto_dynamodb::{AttributeDefinition, CreateTableError, CreateTableInput, DescribeTableError, DescribeTableInput, DynamoDb, DynamoDbClient, GetItemInput, GlobalSecondaryIndex, KeySchemaElement, ListTablesInput, LocalSecondaryIndex, Projection, ProvisionedThroughput, PutItemInput, QueryInput};
 use tracing::{debug, error, info, instrument, warn};
 use uuid::Uuid;
 
@@ -19,7 +16,7 @@ use uuid::Uuid;
 
 #[derive(Attributes, Debug, Clone)]
 pub struct Author {
-    id: Uuid,
+    // id: Uuid,
     #[dynomite(default)]
     name: String,
 }
@@ -27,16 +24,18 @@ pub struct Author {
 #[derive(Item, Debug, Clone)]
 pub struct Book {
     // Уникальный идентификатор
-    #[dynomite(partition_key)]
-    id: Uuid,
+    // #[dynomite(partition_key)]
+    // id: Uuid,
 
     // Вторичный ключ сортировки
-    #[dynomite(sort_key)]
-    year: u32,
+    // #[dynomite(sort_key)]
+    // year: u32,
 
-    // Имя книги
-    #[dynomite(rename = "book_title")]
+    // Имя книги + уникальный идентификатор
+    #[dynomite(partition_key, rename = "book_title")]
     title: String,
+
+    book_year: u32,
 
     // Список авторов
     authors: Option<Vec<Author>>,
@@ -78,25 +77,53 @@ async fn create_table_if_needed(client: &impl DynamoDb, table_name: String) -> R
         // Описание ключей
         key_schema: vec![
             KeySchemaElement {
-                attribute_name: "id".to_string(),
+                attribute_name: "book_title".to_string(),
                 key_type: "HASH".to_string(), // Первичный ключ - HASH, ключ сортировки - RANGE
             },
-            KeySchemaElement {
-                attribute_name: "year".to_string(),
-                key_type: "RANGE".to_string(), // Первичный ключ - HASH, ключ сортировки - RANGE
-            },
+            // KeySchemaElement {
+            //     attribute_name: "year".to_string(),
+            //     key_type: "RANGE".to_string(), // Первичный ключ - HASH, ключ сортировки - RANGE
+            // },
         ],
         // Описание типов ключей
         attribute_definitions: vec![
             AttributeDefinition {
-                attribute_name: "id".to_string(),
+                attribute_name: "book_title".to_string(),
                 attribute_type: "S".to_string(),
             },
             AttributeDefinition {
-                attribute_name: "year".to_string(),
+                attribute_name: "book_year".to_string(),
                 attribute_type: "N".to_string(), // [B, N, S]
             },
         ],
+        // Глобальные вторичные индексы нужны для поиска по главному ключу и по какому-то еще полю??
+        global_secondary_indexes: Some(vec![GlobalSecondaryIndex{
+            index_name: "year_index".to_owned(),
+            key_schema: vec![KeySchemaElement{
+                attribute_name: "book_year".to_owned(),
+                key_type: "HASH".to_owned()
+            }],
+            projection: Projection{
+                projection_type: Some("ALL".to_owned()), // Можно указать лишь нужные нам поля из исходной таблицы
+                ..Default::default()
+            },
+            provisioned_throughput: Some(ProvisionedThroughput {
+                read_capacity_units: 1,
+                write_capacity_units: 1,
+            }),
+            ..Default::default()
+        }]),
+        // Вторичные индексы нужны для поиска по главному ключу и по какому-то еще полю??
+        // local_secondary_indexes: Some(vec![
+        //     LocalSecondaryIndex{
+        //         index_name: "year_index".to_owned(),
+        //         key_schema: vec![KeySchemaElement{
+        //             attribute_name: "year".to_owned(),
+        //             key_type: "HASH".to_owned()
+        //         }],
+        //         ..Default::default()
+        //     }
+        // ]),
         // Количество записей и чтения на отдельную таблицу в секунду
         provisioned_throughput: Some(ProvisionedThroughput {
             read_capacity_units: 1,
@@ -115,7 +142,7 @@ async fn create_table_if_needed(client: &impl DynamoDb, table_name: String) -> R
             debug!(table_create_result = ?result);
         }
         Err(RusotoError::Service(CreateTableError::ResourceInUse(err))) => {
-            debug!(%err, "Table already exists, creating is not required");
+            debug!(%err, "Table already exists, creating is not required:");
         }
         Err(err) => {
             return Err(err).wrap_err("Table create");
@@ -133,26 +160,22 @@ async fn create_table_if_needed(client: &impl DynamoDb, table_name: String) -> R
             })
             .await?
             .table
-            .ok_or_else(||{
-                eyre::eyre!("Table info request failed")
-            })?
+            .ok_or_else(|| eyre::eyre!("Table info request failed"))?
             .table_status
-            .ok_or_else(||{
-                eyre::eyre!("Table status is missing")
-            })?;
+            .ok_or_else(|| eyre::eyre!("Table status is missing"))?;
 
         // Узнаем статус
         match status.as_str() {
             "ACTIVE" => {
                 info!("Table is available now");
-                break;                
-            },
+                break;
+            }
             status @ _ => {
-                if check_count < 5 {
-                    info!(status, "Table will be awailable soon, awaiting");
+                if check_count < 60 {
+                    info!(status, "Table will be awailable soon, awaiting:");
                     tokio::time::delay_for(std::time::Duration::from_secs(1)).await;
                     check_count += 1;
-                }else{
+                } else {
                     return Err(eyre::eyre!("Table available check timeout, status: {}", status));
                 }
             }
@@ -164,7 +187,7 @@ async fn create_table_if_needed(client: &impl DynamoDb, table_name: String) -> R
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#[instrument(skip(client, book), fields(book_id = %book.id))]
+#[instrument(skip(client, book), fields(book_title = %book.title))]
 async fn insert_book(client: &impl DynamoDb, table_name: String, book: Book) -> Result<(), eyre::Error> {
     let input = PutItemInput {
         table_name,
@@ -174,7 +197,7 @@ async fn insert_book(client: &impl DynamoDb, table_name: String, book: Book) -> 
 
     let result = client.put_item(input).await?;
 
-    info!(book_result = ?result, "book insert success");
+    info!(book_result = ?result, "book insert success:");
 
     Ok(())
 }
@@ -191,6 +214,7 @@ async fn get_book_with_title(
         .get_item(GetItemInput {
             table_name,
             key: book_key,
+            // projection_expression: Some("title".to_owned()), // Выдергивание лишь определенных полей
             ..Default::default()
         })
         .await?
@@ -198,8 +222,51 @@ async fn get_book_with_title(
 
     match result {
         Some(data) => {
-            let book = Book::from_attrs(data)?;
+            let book = Book::from_attrs(data).wrap_err("Book parsing")?;
             Ok(Some(book))
+        }
+        None => Ok(None),
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[instrument(skip(client))]
+async fn query_books_with_year_greater(
+    client: &impl DynamoDb,
+    table_name: String,
+    index_name: String,
+    year: u64,
+) -> Result<Option<Vec<Book>>, eyre::Error> {
+    let result = client
+        .query(QueryInput {
+            table_name,
+            index_name: Some(index_name),
+            // key_condition_expression: Some("id = :id and begins_with(title, :t)".to_owned()),
+            // expression_attribute_values: Some(attr_map! {
+            //     ":id" => id,
+            //     ":t" => book_title_begin
+            // }),
+            key_condition_expression: Some("book_year = :y".to_owned()),
+            expression_attribute_values: Some(attr_map! {
+                ":y" => year
+            }),
+            ..Default::default()
+        })
+        .await?
+        .items;
+
+    match result {
+        Some(items_data) => {
+            let mut result = Vec::new();
+            result.reserve(items_data.len());
+
+            for data in items_data.into_iter() {
+                let book = Book::from_attrs(data).wrap_err("Book parsing")?;
+                result.push(book);
+            }
+
+            Ok(Some(result))
         }
         None => Ok(None),
     }
@@ -226,11 +293,9 @@ pub async fn test_dynamo_db() -> Result<(), eyre::Error> {
 
     // Добавляем книжку
     let book = Book {
-        id: Uuid::new_v4(),
-        year: 1999,
         title: "first_book_title".to_string(),
+        book_year: 1999,
         authors: Some(vec![Author {
-            id: Uuid::new_v4(),
             name: "Book author title".to_string(),
         }]),
     };
@@ -239,7 +304,11 @@ pub async fn test_dynamo_db() -> Result<(), eyre::Error> {
 
     // Пытаемся получить эту книжку
     let received_book = get_book_with_title(&client, table_name.to_owned(), new_book_key).await?;
-    info!(?received_book, "Book received");
+    info!(?received_book, "Book received:");
+
+    // Делаем запрос списка
+    let query_res = query_books_with_year_greater(&client, table_name.to_owned(), "year_index".to_owned(), 1999_u64).await?;
+    info!(?query_res, "Book search received:");
 
     Ok(())
 }
