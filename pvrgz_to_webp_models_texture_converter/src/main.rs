bc9976f76ad73b2d519fb3465dcab0eaa174758a
Pavel Ershov <devnulpavel@gmail.com>
2021-06-16 16:19:28 +0300

    Image validation

--- pvrgz_to_webp_models_texture_converter/src/main.rs
+++ pvrgz_to_webp_models_texture_converter/src/main.rs
@@ -11,7 +11,7 @@ use std::{
     fs::{remove_file, File},
     io::{Read, Write},
     path::{Path, PathBuf},
-    sync::Mutex,
+    sync::RwLock,
 };
 use structopt::StructOpt;
 use tracing::{debug, instrument, Level};
@@ -25,19 +25,18 @@ fn setup_logging(arguments: &AppArguments) {
 
     // Настройка логирования на основании количества флагов verbose
     let level = match arguments.verbose {
-        0 => Level::ERROR,
-        1 => Level::WARN,
-        2 => Level::INFO,
-        3 => Level::DEBUG,
-        4 => Level::TRACE,
+        0 => Level::WARN,
+        1 => Level::INFO,
+        2 => Level::DEBUG,
+        3 => Level::TRACE,
         _ => {
-            panic!("Verbose level must be in [0, 4] range");
+            panic!("Verbose level must be in [0, 3] range");
         }
     };
     tracing_subscriber::registry()
         .with(tracing_subscriber::filter::LevelFilter::from_level(level))
         .with(tracing_subscriber::filter::EnvFilter::new(env!("CARGO_PKG_NAME"))) // Логи только от текущего приложения, без библиотек
-        .with(tracing_subscriber::fmt::layer().pretty())
+        .with(tracing_subscriber::fmt::layer())
         .with(tracing_error::ErrorLayer::default()) // Для поддержки захватывания SpanTrace в eyre
         .try_init()
         .expect("Tracing init failed");
@@ -53,15 +52,15 @@ fn validate_arguments(arguments: &AppArguments) {
 }
 
 #[instrument(level = "error", skip(models_json_string, path_root, pvrgz_path))]
-fn replace_pvrgz_to_webp_path(models_json_string: &Mutex<String>, path_root: &Path, pvrgz_path: &Path) -> Result<(), eyre::Error> {
-    // Заменяем старый путь в json на новый c новым расширением
-    let old_path = pvrgz_path.strip_prefix(path_root).wrap_err("Pvrgz path root strip")?;
-    let old_path_str = old_path.to_str().ok_or_else(|| eyre::eyre!("Old path to string failed"))?;
-    let new_path = old_path.with_extension("webp");
-    let new_path_str = new_path.to_str().ok_or_else(|| eyre::eyre!("New path to string failed"))?;
+fn replace_pvrgz_to_webp_path(models_json_string: &RwLock<String>, path_root: &Path, pvrgz_path: &Path) -> Result<(), eyre::Error> {
+    // Относительный путь
+    let old_relative_path = pvrgz_path.strip_prefix(path_root).wrap_err("Pvrgz path root strip")?;
+    let old_relative_path_str = old_relative_path.to_str().ok_or_else(|| eyre::eyre!("Old path to string failed"))?;
+    let new_relative_path = old_relative_path.with_extension("webp");
+    let new_relative_path_str = new_relative_path.to_str().ok_or_else(|| eyre::eyre!("New path to string failed"))?;
 
     // Получаем блокировку на строке
-    let mut data_guard = match models_json_string.lock() {
+    let mut data_guard = match models_json_string.write() {
         Ok(data_guard) => data_guard,
         Err(err) => {
             return Err(eyre::eyre!("Mutex lock error: {}", err));
@@ -71,7 +70,7 @@ fn replace_pvrgz_to_webp_path(models_json_string: &Mutex<String>, path_root: &Pa
     // Обновляем строку на новое значение пути
     // TODO: Очень много переаллокаций, может быть использовать предаллоцированную арену?
     // bumpalo? slab?
-    *data_guard = data_guard.replace(old_path_str, new_path_str);
+    *data_guard = data_guard.replace(old_relative_path_str, new_relative_path_str);
 
     Ok(())
 }
@@ -81,7 +80,7 @@ fn process_found_path(
     cache_info: &CacheInfo,
     utils_pathes: &UtilsPathes,
     arguments: &AppArguments,
-    models_json_string: &Mutex<String>,
+    models_json_string: &RwLock<String>,
     path_root: &Path,
     pvrgz_path: PathBuf,
 ) -> Result<(), eyre::Error> {
@@ -135,7 +134,7 @@ fn main() {
             .expect("File data read failed");
 
         // Завернем данные в Mutex для возможности изменять из разных потоков
-        Mutex::new(data)
+        RwLock::new(data)
     };
 
     // Идем по всем нашим директориям
@@ -148,7 +147,7 @@ fn main() {
         // Только валидные папки и файлики
         .map(|(root, entry)| (root, entry.expect("Entry path err").into_path()))
         // Фильтруем только .pvrgz
-        .filter(|(_, path)| {
+        .filter(|(root, path)| {
             // Только файлики
             if !path.is_file() {
                 return false;
@@ -164,6 +163,25 @@ fn main() {
             let meta = std::fs::metadata(&path).expect("File metadata read failed");
             if meta.len() < arguments.minimum_pvrgz_size {
                 return false;
+            }
+
+            // Проверим сначала, что данная текстура у нас встречается вообще в файлике json
+            {
+                // TODO: Повторно вычисляется ниже, не дублировать?
+                let old_relative_path_str = path
+                    .strip_prefix(root)
+                    .expect("Root strip error")
+                    .to_str()
+                    .expect("Path to str err");
+
+                // Получаем блокировку на строке
+                let data_guard = json_file_data.read().expect("Mutex lock failed");
+
+                // Если не нашли, выходим с выводом предупреждения
+                if data_guard.find(old_relative_path_str).is_none() {
+                    // warn!(path = old_relative_path_str, "File not found in models.json");
+                    return false;
+                }
             }
 
             true
@@ -182,7 +200,7 @@ fn main() {
     // После конвертации сохраняем новое содержимое .json файлика
     {
         let mut file = File::create(arguments.models_config_json).expect("Json file open failed");
-        let data_guard = json_file_data.lock().expect("Mutex lock failed");
+        let data_guard = json_file_data.read().expect("Mutex lock failed");
         file.write_all(data_guard.as_bytes()).expect("Json data save failed");
     }
 }
