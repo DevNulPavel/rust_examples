3afb20f04916c820f4114486e2982ae652e3043c
Pavel Ershov <pershov@game-insight.com>
2021-11-16 10:43:35 +0300

    REfactoring

--- file_upload_proxy/src/main.rs
+++ file_upload_proxy/src/main.rs
@@ -1,45 +1,27 @@
 mod app_arguments;
+mod error;
 
-use eyre::WrapErr;
-use tracing::{
-    instrument,
-    error,
-    debug,
-    info,
-    warn,
-};
-use futures::{future::pending};
-use structopt::{
-    StructOpt
-};
-use std::{
-    net::{
-        SocketAddr
-    },
-    convert::{
-        Infallible
-    }
-};
-use tokio::{
-    sync::{
-        broadcast,
-        mpsc
-    }
-};
-use hyper::{
-    service::{
-        make_service_fn,
-        service_fn
-    },
-    server::{conn::AddrStream, Server},
-};
 use crate::{
-    app_arguments::{
-        AppArguments
-    }
+    app_arguments::AppArguments,
+    error::{ErrorWithStatusAndDesc, WrapErrorWithStatusAndDesc},
 };
+use eyre::WrapErr;
+use futures::{future::pending};
+use hyper::{
+    body::{to_bytes, Body as BodyStruct, Bytes},
+    http::{header, method::Method, status::StatusCode},
+    server::{conn::AddrStream, Server},
+    service::{make_service_fn, service_fn},
+    Request, Response,
+};
+use std::{convert::Infallible, net::SocketAddr, process::exit};
+use structopt::StructOpt;
+use tracing::{debug, error, info, instrument, trace, warn};
+use tracing_futures::Instrument;
 
-fn initialize_logs(arguments: &AppArguments) {
+fn initialize_logs(arguments: &AppArguments) -> Result<(), eyre::Error> {
+    use tracing_subscriber::prelude::*;
+
     let level = match arguments.verbose {
         0 => tracing::Level::ERROR,
         1 => tracing::Level::WARN,
@@ -64,18 +46,40 @@ fn initialize_logs(arguments: &AppArguments) {
     let full_subscriber = tracing_subscriber::registry().with(filter).with(error_layer).with(stdoud_sub);
 
     // Враппер для библиотеки log
-    tracing_log::LogTracer::init().expect("Log wrapper create failed");
+    tracing_log::LogTracer::init().wrap_err("Log wrapper create failed")?;
 
     // Установка по-умолчанию
-    tracing::subscriber::set_global_default(full_subscriber).expect("Global subscriber set failed");
+    tracing::subscriber::set_global_default(full_subscriber).wrap_err("Global subscriber set failed")?;
+
+    Ok(())
 }
 
-#[instrument(level = "error", skip(cancel))]
-async fn service_handler(
-    remove_addr: SocketAddr,
-    req: Request<BodyStruct>,
-    cancel: Cancelation,
-) -> Result<Response<BodyStruct>, eyre::Error> {
+fn response_with_status_and_empty_body(status: StatusCode) -> Response<BodyStruct> {
+    Response::builder()
+        .status(status)
+        .body(BodyStruct::empty())
+        .expect("Static fail response create failed") // Статически создаем ответ, здесь не критично
+}
+
+fn response_with_status_and_error(status: StatusCode, err_desc: &str) -> Response<BodyStruct> {
+    let error_json = format!(r#"{{"description": "{}"}}"#, err_desc);
+    Response::builder()
+        .status(status)
+        .body(BodyStruct::from(error_json))
+        .expect("Static fail response create failed") // Статически создаем ответ, здесь не критично
+}
+
+fn response_with_status_desc_and_trace_id(status: StatusCode, err_desc: &str, trace_id: &str) -> Response<BodyStruct> {
+    let error_json = format!(r#"{{"error_trace_id": "{}", "desc": "{}"}}"#, trace_id, err_desc);
+    Response::builder()
+        .status(status)
+        .body(BodyStruct::from(error_json))
+        .expect("Static fail response create failed") // Статически создаем ответ, здесь не критично
+}
+
+// Трассировка настраивается уровнем выше
+// #[instrument(level = "error")]
+async fn service_handler(req: Request<BodyStruct>) -> Result<Response<BodyStruct>, ErrorWithStatusAndDesc> {
     // debug!("Request processing begin");
 
     match (req.method(), req.uri().path()) {
@@ -85,7 +89,8 @@ async fn service_handler(
             let response = Response::builder()
                 .status(StatusCode::MOVED_PERMANENTLY)
                 .header(header::LOCATION, "/help")
-                .body(BodyStruct::empty())?;
+                .body(BodyStruct::empty())
+                .wrap_err_with_500()?;
 
             Ok(response)
         }
@@ -95,102 +100,54 @@ async fn service_handler(
             info!("Help");
             let response = Response::builder()
                 .status(StatusCode::OK)
-                .body(BodyStruct::from("Try to send POST request at '/echo'"))?;
+                .body(BodyStruct::from("Try to send POST request at '/echo'"))
+                .wrap_err_with_500()?;
             Ok(response)
         }
-
-        // Возвращаем все в ответ
-        (&Method::POST, "/echo") => {
-            info!("Echo");
-            let resp = Response::builder().status(StatusCode::OK).body(req.into_body())?;
-            Ok(resp)
-        }
-
-        // Возвращаем все в ответ в верхнем регистре
-        (&Method::POST, "/echo/uppercase") => {
-            info!("Echo uppercase");
-
-            let new_stream = req.into_body().map_ok(|data_chunk| {
-                let result: Bytes = data_chunk.into_iter().map(|val| val.to_ascii_uppercase()).collect();
-                result
-            });
-
-            let resp = Response::builder()
-                .status(StatusCode::OK)
-                .body(BodyStruct::wrap_stream(new_stream))
-                .wrap_err("Body build")?;
-            Ok(resp)
-        }
-
-        // Возвращаем все в ответ в обратном порядке
-        (&Method::POST, "/echo/reverse") => {
-            info!("Echo reverse");
-
-            let body_data = to_bytes(req.into_body())
-                .await
-                .wrap_err_with_status(StatusCode::BAD_GATEWAY, "Body to bytes convert failed".into())?;
-
-            let rev_body_data: Bytes = body_data.iter().rev().cloned().collect();
-
-            let resp = Response::builder()
-                .status(StatusCode::OK)
-                .body(BodyStruct::from(rev_body_data))
-                .wrap_err("Body build")?;
-            Ok(resp)
-        }
-
-        // Возвращаем все в ответ в обратном порядке
-        (&Method::GET, "/web_socket") => process_web_socket(req, cancel).await,
 
         // Любой другой запрос
         _ => {
             warn!("Invalid request");
-            let response = Response::builder().status(StatusCode::NOT_FOUND).body(BodyStruct::empty()).unwrap();
+            let response = Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(BodyStruct::empty())
+                .wrap_err_with_500()?;
             Ok(response)
         }
     }
-}
-
-async fn shutdown_signal_wait() {
-    // Wait for the CTRL+C signal
-    if let Err(err) = tokio::signal::ctrl_c().await {
-        warn!("Shutdown signal awaiter setup failed, continue without: {}", err);
-        // Создаем поэтому вечную future
-        pending::<()>().await;
-    }
-    info!("Shutdown signal received");
 }
 
 async fn run_server(app_arguments: AppArguments) -> Result<(), eyre::Error> {
     let addr = SocketAddr::from(([0, 0, 0, 0], app_arguments.port));
 
-    // Канал для отправки всем завершения работы
-    let (finish_sender, finish_receiver) = broadcast::channel::<mpsc::Sender<()>>(1);
-    drop(finish_receiver);
-
     // Сервис необходим для каждого соединения, поэтому создаем враппер, который будет генерировать наш сервис
     let make_svc = make_service_fn(|conn: &AddrStream| {
+        // Получаем адрес удаленного подключения
         let remote_addr = conn.remote_addr();
-        let sender = finish_sender.clone();
+
         async move {
             // Создаем сервис из функции с помощью service_fn
             Ok::<_, Infallible>(service_fn(move |req| {
-                let receiver = sender.subscribe();
                 async move {
-                    match service_handler(remote_addr, req, finish_receiver).await {
+                    // Создаем идентификатор трассировки для отслеживания ошибок в общих логах
+                    let trace_id = format!("{:x}", uuid::Uuid::new_v4());
+
+                    // Создаем span с идентификатором трассировки
+                    let span = tracing::error_span!("request", 
+                        remote_ip = %remote_addr, 
+                        trace_id = %trace_id,
+                        path = req.uri().path());
+
+                    // Обработка сервиса
+                    match service_handler(req).instrument(span).await {
                         resp @ Ok(_) => resp,
                         Err(err) => {
                             // Выводим ошибку в консоль
                             error!("{}", err);
 
-                            // Ошибка с дополнительной инфой?
-                            let resp = if let Some(http_err) = err.downcast_ref::<ErrorWithStatusAndDesc>() {
-                                // Создаем ответ с правильным статусом
-                                response_with_status_end_error(http_err.status, http_err.desc.as_ref())
-                            } else {
-                                // Создаем ответ со стандартным статусом
-                                response_with_status_and_empty_body(StatusCode::INTERNAL_SERVER_ERROR)
-                            };
+                            // Ответ в виде ошибки
+                            let resp = response_with_status_desc_and_trace_id(err.status, &err.desc, &trace_id);
+
                             Ok(resp)
                         }
                     }
@@ -202,19 +159,36 @@ async fn run_server(app_arguments: AppArguments) -> Result<(), eyre::Error> {
     // Создаем сервер c ожиданием завершения работы
     Server::bind(&addr)
         .serve(make_svc)
-        .with_graceful_shutdown(shutdown_signal_wait())
+        .with_graceful_shutdown(async {
+            // Wait for the CTRL+C signal
+            if let Err(err) = tokio::signal::ctrl_c().await {
+                warn!("Shutdown signal awaiter setup failed, continue without: {}", err);
+                // Создаем поэтому вечную future
+                pending::<()>().await;
+            }
+            info!("Shutdown signal received");
+        })
         .await
         .wrap_err("Server awaiting fail")?;
 
-    // Ждем здесь завершения всех заспавненых обработчиков вебсокетов
-    // Создаем канал для ожидания завершения, как только все отправители уничтожены -
-    // получатель возвращает ошибку, но мы ее игнорим.
-    // Некий аналог WaitGroup
-    // https://tokio.rs/tokio/topics/shutdown
-    let (wait_sender, mut wait_receiver) = mpsc::channel(1);
-    finish_sender.send(wait_sender).wrap_err("Awaiters complete send err")?;
-    let _ = wait_receiver.recv().await;
-    info!("All clients processing complete");
+    Ok(())
+}
+
+fn execute_app() -> Result<(), eyre::Error> {
+    // Аргументы приложения
+    let app_arguments = AppArguments::from_args_safe().wrap_err("App arguments parsing")?;
+
+    // Логи
+    initialize_logs(&app_arguments).wrap_err("Logs init")?;
+
+    // Создаем рантайм для работы сервера
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .wrap_err("Tokio runtime build")?;
+
+    // Стартуем сервер
+    runtime.block_on(run_server(app_arguments)).wrap_err("Server running fail")?;
 
     Ok(())
 }
@@ -222,19 +196,11 @@ async fn run_server(app_arguments: AppArguments) -> Result<(), eyre::Error> {
 fn main() {
     // Бектрейсы в ошибках
     color_eyre::install().expect("Color eyre initialize failed");
-    
-    // Аргументы приложения
-    let app_arguments = AppArguments::from_args();
 
-    // Логи
-    initialize_logs(&app_arguments);
-
-    // Создаем рантайм для работы сервера
-    let runtime = tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .expect("Tokio runtime build");
-
-    // Стартуем сервер
-    runtime.block_on(run_server(app_arguments)).expect("Server running fail");
+    if let Err(err) = execute_app() {
+        eprintln!("{:?}", err);
+        exit(1);
+    } else {
+        exit(0);
+    }
 }
