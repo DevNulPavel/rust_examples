7ae2670ec11073a19771a4aa9757746262db1503
Pavel Ershov <devnulpavel@gmail.com>
2022-10-12 14:41:53 +0300

    Redis benchmark

---
+++ test86_redis_test/src/main.rs
@@ -0,0 +1,38 @@
+use redis::{Client, Commands};
+
+macro_rules! format_small {
+    ($len:expr, $($arg:tt)*) => {
+        {
+            use std::fmt::Write;
+            let mut buf = smallstr::SmallString::<[u8; $len]>::new();
+            buf.write_fmt(::core::format_args!($($arg)*)).unwrap();
+            buf
+        }
+    };
+}
+
+fn main() {
+    // redis-server --unixsocket /Users/devnul/redis.sock --unixsocketperm 775
+
+    //let client = Client::open("redis://127.0.0.1:6379").unwrap();
+    let client = Client::open("redis+unix:///Users/devnul/redis.sock").unwrap();
+
+    let mut conn = client.get_connection().unwrap();
+
+    /*for i in 0..4_000_000 {
+        let key = format_small!(32, "u:{}", i);
+        conn.set::<_, _, ()>(key.as_str(), i).unwrap();
+
+        if i % 100_000 == 0 {
+            println!("{}", i);
+        }
+    }*/
+
+    /*for i in 0..4_000_000 {
+        conn.hset::<_, _, _, ()>("users", i, i).unwrap();
+
+        if i % 100_000 == 0 {
+            println!("{}", i);
+        }
+    }*/
+}
