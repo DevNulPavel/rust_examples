22cc3c420d3b1a71b8ccc25700430469f71a2240
DevNul <devnulpavel@gmail.com>
2023-06-26 19:30:45 +0300

    Redis module examples 5

--- test89_redis_module/examples/data_type.rs
+++ test89_redis_module/examples/data_type.rs
@@ -14,7 +14,7 @@ struct MyType {
 ////////////////////////////////////////////////////////////////////////////////////////
 
 /// Создаем статическую переменную, которая содержит описание нашего нового типа данных.
-/// Подробнее можно почитать здесь по пункту `RedisModule_CreateDataType`: 
+/// Подробнее можно почитать здесь по пункту `RedisModule_CreateDataType`:
 /// - https://redis.io/docs/reference/modules/modules-api-ref/
 /// - https://redis.io/docs/reference/modules/modules-native-types/
 static MY_REDIS_TYPE: RedisType = RedisType::new(
@@ -23,17 +23,17 @@ static MY_REDIS_TYPE: RedisType = RedisType::new(
     // При ошибке, u64 снова конвертируется в строку и выдается подсказка при ошибке или отсутствии модуля.
     // Так же этот код выдается при вызове команды TYPE.
     //
-    // Почему 9 символов, а не 8? 
+    // Почему 9 символов, а не 8?
     // Возможно, что дело в том, что используется представление 127 символов ASCII, а не 255,
-    // поэтому появляется запас для одного лишнего символа еще. 
+    // поэтому появляется запас для одного лишнего символа еще.
     // Один символ - 6 бит. Остается при сохранении еще 10 бит на версию типа.
     // Иначе говоря: 6 * 9 + 10 = 64
     //
-    // The type name is a 9 character name in the character 
-    // set that includes from A-Z, a-z, 0-9, plus 
+    // The type name is a 9 character name in the character
+    // set that includes from A-Z, a-z, 0-9, plus
     // the underscore _ and minus - characters.
     "mytype123",
-    // Версия нашего типа, нужно для возмиожности подгрузки из 
+    // Версия нашего типа, нужно для возмиожности подгрузки из
     // бекапов данных о типах старшей версии.
     0,
     // Различные метода нашего нового типа для редиса.
@@ -74,39 +74,81 @@ static MY_REDIS_TYPE: RedisType = RedisType::new(
     },
 );
 
+////////////////////////////////////////////////////////////////////////////////////////
+
+// Load
+// fn(*mut RedisModuleIO, i32) -> *mut c_void
+
+// Save
+// fn(*mut RedisModuleIO, *mut c_void)
+
+/// Данная сишная функция у нас вызывается при попытке очистить память нашей структурой
 unsafe extern "C" fn free(value: *mut c_void) {
-    drop(Box::from_raw(value.cast::<MyType>()));
+    // Сначала кастим сишный указатель к нашему конкретному типу
+    let value_obj = value.cast::<MyType>();
+
+    // Теперь преобразуем в box
+    let boxed_value = Box::from_raw(value_obj);
+
+    // Теперь явно уничтожаем память с помощью drop у box
+    drop(boxed_value);
 }
 
+////////////////////////////////////////////////////////////////////////////////////////
+
+/// Обработчик команды
 fn alloc_set(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
+    // Получаем аргументы нашей команды, пропускаем самую первую, так как это сама команда
     let mut args = args.into_iter().skip(1);
+
+    // Получаем параметр ключа
     let key = args.next_arg()?;
+
+    // Получаем размер в виде цифры
     let size = args.next_i64()?;
 
+    // Делаем отладочный вывод
     ctx.log_debug(format!("key: {key}, size: {size}").as_str());
 
-    let key = ctx.open_key_writable(&key);
+    // Теперь по данному ключу мы открываем на запись буфер
+    let key_write_guard = ctx.open_key_writable(&key);
 
-    if let Some(value) = key.get_value::<MyType>(&MY_REDIS_TYPE)? {
+    // Здесь мы можем получить попытаться мутабельную ссылку на нашу структуру
+    // с помощью передачи в качестве параметра информации о типе.
+    if let Some(value) = key_write_guard.get_value::<MyType>(&MY_REDIS_TYPE)? {
+        // Если у нас такое значение уже было, заполняем его значениями B много раз
         value.data = "B".repeat(size as usize);
     } else {
+        // Если такого значения еще у нас не было, тогда мы создаем структурку
+        // с нуля и заполняем значениями
         let value = MyType {
             data: "A".repeat(size as usize),
         };
 
-        key.set_value(&MY_REDIS_TYPE, value)?;
+        // Записываем это самое значение с помощью указания типа переменной
+        key_write_guard.set_value(&MY_REDIS_TYPE, value)?;
     }
     Ok(size.into())
 }
 
+// Обработчик получения данных по ключу
 fn alloc_get(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
+    // Получаем аргументы и пропускаем саму команду
     let mut args = args.into_iter().skip(1);
+
+    // Получаем теперь сам ключ
     let key = args.next_arg()?;
 
-    let key = ctx.open_key(&key);
+    // Открываем ключ данный на чтение
+    let key_read_guard = ctx.open_key(&key);
 
-    let value = match key.get_value::<MyType>(&MY_REDIS_TYPE)? {
-        Some(value) => value.data.as_str().into(),
+    // Пытаемся получить значение по данному ключу с помощью каста к типу + описания типа
+    let value = match key_read_guard.get_value::<MyType>(&MY_REDIS_TYPE)? {
+        Some(value) => {
+            // Раз получили ссылку на данные, тогда можем извлечь и содержимое
+            value.data.as_str().into()
+        },
+        // Либо возвращаем пустоту
         None => ().into(),
     };
 
