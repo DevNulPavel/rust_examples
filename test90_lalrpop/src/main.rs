a3464d1ce64ebeec0e83328ccc7aa049a73acf7e
DevNul <devnulpavel@gmail.com>
2023-07-01 08:56:41 +0300

    Lalrpop research

--- test90_lalrpop/src/main.rs
+++ test90_lalrpop/src/main.rs
@@ -1,14 +1,14 @@
 use lalrpop_util::lalrpop_mod;
 
-lalrpop_mod!(syntax);
+lalrpop_mod!(basic);
 
 fn main() {
-    assert_eq!(syntax::TermParser::new().parse("123").unwrap(), 123);
-    assert_eq!(syntax::TermParser::new().parse("(123)").unwrap(), 123);
+    assert_eq!(basic::TermParser::new().parse("123").unwrap(), 123);
+    assert_eq!(basic::TermParser::new().parse("(123)").unwrap(), 123);
 
-    assert!(syntax::TermParser::new().parse("((123a))").is_err());
-    assert!(syntax::TermParser::new().parse("((123)").is_err());
+    assert!(basic::TermParser::new().parse("((123a))").is_err());
+    assert!(basic::TermParser::new().parse("((123)").is_err());
     
     // Continue:
-    // http://lalrpop.github.io/lalrpop/tutorial/004_full_expressions.html
+    // http://lalrpop.github.io/lalrpop/tutorial/005_building_asts.html
 }
