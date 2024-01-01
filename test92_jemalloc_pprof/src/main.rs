6089b728e3001b5c6cb0af57e09d505425516fc8
DevNul <devnulpavel@gmail.com>
2024-01-01 17:31:45 +0300

    Jemalloc pprof research

---
+++ test92_jemalloc_pprof/src/main.rs
@@ -0,0 +1,10 @@
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[allow(non_upper_case_globals)]
+#[export_name = "malloc_conf"]
+pub static malloc_conf: &[u8] = b"prof:true,prof_active:true,lg_prof_sample:19\0";
+
+fn main() {}
