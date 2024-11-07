914fc07c76092d03df9eb908f94e3cf733e9bafb
Pavel Ershov <devnulpavel@gmail.com>
2024-11-07 16:51:32 +0300

    Research

--- external/092_rust-ipc/src/shmem.rs
+++ external/092_rust-ipc/src/shmem.rs
@@ -15,19 +15,31 @@ use std::{
 
 ////////////////////////////////////////////////////////////////////////////////
 
+/// Обертка для удобной работы с общей памятью
 pub struct ShmemWrapper {
+    /// Непосредственно указатель на общие данные
     pub shmem: Shmem,
+
+    /// Являемся ли мы владельцем текущей общей памяти?
     pub owner: bool,
+
+    /// Флаги в общей памяти для блокировок
     pub our_event: Box<dyn EventImpl>,
+
+    /// Флаги в общей памяти для блокировок
     pub their_event: Box<dyn EventImpl>,
+
+    /// С какого момента начинаются пользовательские данные
     pub data_start: usize,
+
+    // Замер общей памяци в целом, включая начальные блокировки
     pub data_size: usize,
 }
 
 impl ShmemWrapper {
     /// Создаем
     pub fn new(handle: Option<String>, data_size: usize) -> ShmemWrapper {
-        // Размер данных + 4 байта для мета-информации
+        // Размер данных + 4 байта для мета-информации и для блокировок
         let data_size = data_size + 4;
 
         // Если нам передан внешний хендл для общей памяти, то мы присоединяемся
@@ -53,58 +65,87 @@ impl ShmemWrapper {
         // Получаем теперь слайс на байты общей памяти
         let bytes = unsafe { shmem.as_slice_mut() };
 
-        // The two events are locks - one for each side. Each side activates the lock while it's
-        // writing, and then unlocks when the data can be read
+        // Два события блокировки - по одному для каждой стороны.
+        // Каждая сторона активирует блокировку перед записью, а потом снимает блокировку
+        // когда данные могут быть прочитаны.
         let ((our_event, lock_bytes_ours), (their_event, lock_bytes_theirs)) = unsafe {
+            // Являемся ли мы владельцами текущей памяти изначально?
             if is_owner {
                 (
+                    // Создаем блокировку на 0-м байте данных
                     BusyEvent::new(bytes.get_mut(0).unwrap(), true).unwrap(),
+                    // Создаем блокировку на 2-м байте данных
                     BusyEvent::new(bytes.get_mut(2).unwrap(), true).unwrap(),
                 )
             } else {
                 (
-                    // If we're not the owner, the events have been created already
+                    // Раз мы не являемся исходным владельцем памяти, то события могут быть уже созданы
+                    //
+                    // Создаем блокировку здесь уже на 2-м байте данных
                     BusyEvent::from_existing(bytes.get_mut(2).unwrap()).unwrap(),
+                    // Создаем блокировку здесь уже на 0-м байте данных
                     BusyEvent::from_existing(bytes.get_mut(0).unwrap()).unwrap(),
                 )
             }
         };
-        // Confirm that we've correctly indexed two bytes for each lock
+
+        // Отвалидируем, что у нас корректный список блокировок для каждой из сторон
         assert!(lock_bytes_ours <= 2);
         assert!(lock_bytes_theirs <= 2);
+
+        // Если мы являемся владельцем, тогда
+        // для каждой из блокировок устанавливаем
+        // событие очистки
         if is_owner {
             our_event.set(EventState::Clear).unwrap();
             their_event.set(EventState::Clear).unwrap();
         }
+
+        // Создаем обертку
         ShmemWrapper {
+            // Непосредственно указатель на общие данные
             shmem,
+            // Являемся ли мы владельцем текущей общей памяти?
             owner: is_owner,
+            // Флаги в общей памяти для блокировок
             our_event,
             their_event,
+            // С какого момента начинаются уже посльзовательские данные в общей памяти
             data_start: 4,
+            // Замер общей памяци в целом, включая начальные блокировки
             data_size,
         }
     }
 
+    /// Устанавливаем сигнал сброса для текущей стороны
     pub fn signal_start(&mut self) {
         self.our_event.set(EventState::Clear).unwrap()
     }
+
+    /// Сбрасываем сигнал сброса для текущей стороны
     pub fn signal_finished(&mut self) {
         self.our_event.set(EventState::Signaled).unwrap()
     }
 
+    /// Записываем теперь данные в общую память
     pub fn write(&mut self, data: &[u8]) {
+        // Получаем сырой слайс на общие данные
         let bytes = unsafe { self.shmem.as_slice_mut() };
 
+        // Записываем теперь выходные данные по определенному смещению
+        // TODO: Здесь лучше бы сразу проверять выход за границы памяти, а не падать с ошибкой
         for i in 0..data.len() {
             bytes[i + self.data_start] = data[i];
         }
     }
 
+    /// Получаем слайс на все пользовательские данные
     pub fn read(&self) -> &[u8] {
         unsafe { &self.shmem.as_slice()[self.data_start..self.data_size] }
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////
 
 // #[derive(Debug)]
 pub struct ShmemRunner {
